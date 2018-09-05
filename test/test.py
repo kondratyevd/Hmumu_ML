63dcb70b4ac84cac140f0499120e439142c5f26d
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2018-09-05 09:37:20 -0400

    Test file for xte station

--- test/test.py
+++ test/test.py
@@ -4,15 +4,15 @@ from src.classifier import Framework
 
 c = Framework()
 
-inFileDir = "/Users/dmitry/root_files/for_tmva/UF_framework_10k/"
+inFileDir = "/data/dmitry/Hmumu/ntuples/"
 treePath = 'dimuons/tree'
 
-c.add_signal('gluglu', inFileDir+"gluglu.root", 0.006343) # label, path, weight
-c.add_signal('vbf', inFileDir+"vbf.root", 0.000495)
+c.add_signal('gluglu', inFileDir+"H2Mu_gg.root", 0.006343) # label, path, weight
+c.add_signal('vbf', inFileDir+"H2Mu_VBF.root", 0.000495)
 
-c.add_background('dy', inFileDir+"zjets_amc.root", 29.853717)
+c.add_background('dy', inFileDir+"ZJets_AMC.root", 29.853717)
 
-c.set_tree_path('tree')
+c.set_tree_path(treePath)
 
 c.add_variable("muPairs.pt", 	1) #second argument is the number of objects considered
 c.add_variable("muPairs.dEta", 	1) #for example, one muon pair
@@ -22,7 +22,7 @@ c.add_variable("muons.eta", 	2)
 c.add_variable("muons.phi", 	2)
 
 c.add_package("TMVA")
-c.add_transf("N,G,P")
+# c.add_transf("N,G,P")
 
 # c.add_package("Keras")
 
