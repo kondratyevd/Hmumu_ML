6d51596e242e4724a45524cf14bbfedaf3793264
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2018-09-07 14:43:40 -0400

    fix

--- packages/TMVA/tmvaTrainer.py
+++ packages/TMVA/tmvaTrainer.py
@@ -44,11 +44,11 @@ class TMVATrainer(object):
 		for file in self.framework.file_list_s + self.framework.file_list_b:
 			tree = ROOT.TChain(self.framework.treePath)
 			tree.Add(file.path)
-			for i in range(2)#(tree.GetEntries()):
+			for i in range(2):#(tree.GetEntries()):
 				# event = []
 				tree.GetEntry(i)
 				for var in self.framework.variable_list:
-					print tree.GetLeaf(var.name).GetValue(0)
+					print var.name+"%f"%tree.GetLeaf(var.name).GetValue(0)
 
 				SF = (0.5*(tree.IsoMu_SF_3 + tree.IsoMu_SF_4)*0.5*(tree.MuID_SF_3 + tree.MuID_SF_4)*0.5*(tree.MuIso_SF_3 + tree.MuIso_SF_4))
 				weight = tree.PU_wgt*tree.GEN_wgt*SF*file.xSec/file.nOriginalWeighted*40000 # I take lumi=40000 because it doesn't matter as it is applied to all samples
