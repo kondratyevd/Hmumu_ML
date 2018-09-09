9ad47ef8b4a749bd821b35f6fecaad1f0227ef37
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2018-09-09 13:15:54 -0400

    test

--- packages/TMVA/tmvaTrainer.py
+++ packages/TMVA/tmvaTrainer.py
@@ -46,7 +46,7 @@ class TMVATrainer(object):
 			tree = ROOT.TChain(self.framework.treePath)
 			tree.Add(file.path)
 			print tree.GetEntries()
-			for i in range(tree.GetEntries()):
+			for i in range(100):# tree.GetEntries()):
 				event = ROOT.std.vector(ROOT.double)()
 				event.clear()
 				tree.GetEntry(i)
@@ -55,10 +55,10 @@ class TMVATrainer(object):
 					if var.isMultiDim:
 						for j in range(var.itemsAdded):
 							event.push_back( ROOT.Double(tree.GetLeaf("%s"%var.name).GetValue(j)) )
-							print "%s:	%d"%(var.name, ROOT.Double(tree.GetLeaf("%s"%var.name).GetValue(j)))
+							print "%s:	%f"%(var.name, ROOT.Double(tree.GetLeaf("%s"%var.name).GetValue(j)))
 					else:
 						event.push_back( ROOT.Double(tree.GetLeaf(var.name).GetValue()))	
-						print "%s %d"%(var.name, ROOT.Double(tree.GetLeaf(var.name).GetValue()))			
+						print "%s %f"%(var.name, ROOT.Double(tree.GetLeaf(var.name).GetValue()))			
 
 				SF = (0.5*(tree.IsoMu_SF_3 + tree.IsoMu_SF_4)*0.5*(tree.MuID_SF_3 + tree.MuID_SF_4)*0.5*(tree.MuIso_SF_3 + tree.MuIso_SF_4))
 				weight = tree.PU_wgt*tree.GEN_wgt*SF*file.xSec/file.nOriginalWeighted*40000 # I take lumi=40000 because it doesn't matter as it is applied to all samples
