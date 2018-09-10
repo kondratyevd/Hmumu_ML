4ec61d635baf430ff4f8e00af4bc27d07e90f6d3
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2018-09-10 13:52:08 -0400

    fix

--- test/plot_ROC.py
+++ test/plot_ROC.py
@@ -5,7 +5,7 @@ from src.plot_TMVA import *
 
 p = Plotter_TMVA()
 filedir = "/home/dkondra/Hmumu_analysis/Hmumu_ML/output/" 
-p.add_TMVA_method("BDT", filedir, "Run_2018-09-10)12-05-15", "BDTG_UF_v1", "#ff0000")
+p.add_TMVA_method("BDT", filedir, "Run_2018-09-10_12-05-15", "BDTG_UF_v1", "#ff0000")
 # p.add_TMVA_method("BDT2", filedir, "Run_2018-09-06_16-30-51", "BDTG_UF_v1", "#0000ff")
 
 p.get_ROC()
