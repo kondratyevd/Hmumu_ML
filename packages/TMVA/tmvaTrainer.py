24ef159d2ec5ce9867d0be1447e3063757a2b785
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2018-09-09 21:20:16 -0400

    test

--- packages/TMVA/tmvaTrainer.py
+++ packages/TMVA/tmvaTrainer.py
@@ -57,7 +57,7 @@ class TMVATrainer(object):
 							for j in range(var.itemsAdded):
 								if tree.GetLeaf(var.validation).GetValue() > j:
 									try:
-										event.push_back( fabs(ROOT.Double(tree.GetLeaf("%s"%var.name).GetValue(j))))
+										# event.push_back( fabs(ROOT.Double(tree.GetLeaf("%s"%var.name).GetValue(j))))
 										print "Case 1:	%f"%fabs(ROOT.Double(tree.GetLeaf("%s"%var.name).GetValue(j)))
 										print "abs():	%f"%abs(ROOT.Double(tree.GetLeaf("%s"%var.name).GetValue(j)))
 									except:
