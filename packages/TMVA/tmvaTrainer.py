b77c75df279facb7e8b99e0481435b20192dfb7e
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2018-09-12 16:06:04 -0400

    test

--- packages/TMVA/tmvaTrainer.py
+++ packages/TMVA/tmvaTrainer.py
@@ -57,10 +57,16 @@ class TMVATrainer(object):
 				event.clear()
 				tree.GetEntry(i)
 
-				if tree.nMuons < 2:
-					print tree.nMuons
-				muon1 = tree.muons[0]
-				muon2 = tree.muons[1]
+
+				for i, muon in enumerate(tree.muons)
+					if i is 0:
+						muon1 = muon
+					elif i is 1:
+						muon2 = muon
+					else:
+						print i
+						
+				# muon2 = tree.muons[1]
 				muPair = tree.muPairs[0]
 				# print muPair.mass
 
