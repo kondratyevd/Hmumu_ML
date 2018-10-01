99cc253c6cf78d221076a6093b80542438025e28
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2018-10-01 13:01:10 -0400

    Dropout study

--- packages/Keras/keras_models.py
+++ packages/Keras/keras_models.py
@@ -55,19 +55,41 @@ def GetListOfModels(nVar):
 	x = Dropout(0.2)(x)
 	UCSD_model.outputs = Dense(2, name = UCSD_model.name+'_output',  activation='softmax')(x)
 
-	model_50_D_25_D = model_init('model_50_D_25_D', nVar, 2048, 200, 'categorical_crossentropy', 'adam')
-	x = Dense(50, name = model_50_D_25_D.name+'_layer_1', activation='relu')(model_50_D_25_D.inputs)
+	model_50_25 = model_init('model_50_25', nVar, 2048, 200, 'categorical_crossentropy', 'adam')
+	x = Dense(50, name = model_50_25.name+'_layer_1', activation='relu')(model_50_25.inputs)
+	x = Dense(25, name = model_50_25.name+'_layer_2', activation='relu')(x)
+	model_50_25.outputs = Dense(2, name = model_50_25.name+'_output',  activation='softmax')(x)
+
+	model_50_D1_25_D1 = model_init('model_50_D1_25_D1', nVar, 2048, 200, 'categorical_crossentropy', 'adam')
+	x = Dense(50, name = model_50_D1_25_D1.name+'_layer_1', activation='relu')(model_50_D1_25_D1.inputs)
+	x = Dropout(0.1)(x)
+	x = Dense(25, name = model_50_D1_25_D1.name+'_layer_2', activation='relu')(x)
+	x = Dropout(0.1)(x)
+	model_50_D1_25_D1.outputs = Dense(2, name = model_50_D1_25_D1.name+'_output',  activation='softmax')(x)
+
+	model_50_D2_25_D2 = model_init('model_50_D2_25_D2', nVar, 2048, 200, 'categorical_crossentropy', 'adam')
+	x = Dense(50, name = model_50_D2_25_D2.name+'_layer_1', activation='relu')(model_50_D2_25_D2.inputs)
 	x = Dropout(0.2)(x)
-	x = Dense(25, name = model_50_D_25_D.name+'_layer_2', activation='relu')(x)
+	x = Dense(25, name = model_50_D2_25_D2.name+'_layer_2', activation='relu')(x)
 	x = Dropout(0.2)(x)
-	model_50_D_25_D.outputs = Dense(2, name = model_50_D_25_D.name+'_output',  activation='softmax')(x)
+	model_50_D2_25_D2.outputs = Dense(2, name = model_50_D2_25_D2.name+'_output',  activation='softmax')(x)
+
+	model_50_D3_25_D3 = model_init('model_50_D3_25_D3', nVar, 2048, 200, 'categorical_crossentropy', 'adam')
+	x = Dense(50, name = model_50_D3_25_D3.name+'_layer_1', activation='relu')(model_50_D3_25_D3.inputs)
+	x = Dropout(0.3)(x)
+	x = Dense(25, name = model_50_D3_25_D3.name+'_layer_2', activation='relu')(x)
+	x = Dropout(0.3)(x)
+	model_50_D3_25_D3.outputs = Dense(2, name = model_50_D3_25_D3.name+'_output',  activation='softmax')(x)
 
 	list_of_models.append(model_3x20)
 	list_of_models.append(model_2x20)
 	list_of_models.append(test_model)
 	list_of_models.append(UCSD_model)
-	list_of_models.append(model_50_D_25_D)
-	
+	list_of_models.append(model_50_25)
+	list_of_models.append(model_50_D1_25_D1)
+	list_of_models.append(model_50_D2_25_D2)
+	list_of_models.append(model_50_D3_25_D3)
+
 	return list_of_models
 
 
--- test/test_Keras_2016_singleMu.py
+++ test/test_Keras_2016_singleMu.py
@@ -125,7 +125,14 @@ c.set_year("2016")
 # c.add_method("BDTG_UF_v1")
 c.add_package("Keras")
 c.add_method("UCSD_model")
-c.add_method("model_50_D_25_D")
+c.add_method("model_50_25") # no Dropout
+c.add_method("model_50_D1_25_D1") # Dropout 0.1
+c.add_method("model_50_D2_25_D2") # Dropout 0.2
+c.add_method("model_50_D3_25_D3") # Dropout 0.3
+
+
+
+
 c.train_methods()
 
 
