c2b4e977c89fd7f0bb2209d51307b01686e5396b
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2018-10-12 14:42:28 -0400

    pt -> pt/mass

--- test/test_Keras_2016_singleMu.py
+++ test/test_Keras_2016_singleMu.py
@@ -3,7 +3,7 @@ sys.path.append( os.path.dirname( os.path.dirname( os.path.abspath(__file__) ) )
 from src.classifier import Framework
 
 c = Framework()
-comment = "Keras test: UCSD model, variables: HIG-17-019, only 1 jetPair + min/max_dR (mu/dimu and jet) + zepenfeld + singleMu (pT, eta, phi).\n Dropout(0.2)" # change this line for each run
+comment = "Keras test: UCSD model, variables: HIG-17-019, only 1 jetPair + min/max_dR (mu/dimu and jet) + zepenfeld + singleMu (pT/mmm, eta, phi).\n Dropout(0.2)" # change this line for each run
 c.add_comment(comment)
 print comment
 treePath = 'dimuons/tree'
@@ -94,14 +94,14 @@ c.add_variable("zepenfeld",					1)
 
 c.add_variable('muons.eta',					2)
 c.add_variable('muons.phi',					2)
-# c.add_variable("mu1_pt_by_mass",			1)
-# c.add_variable("mu2_pt_by_mass",			1)
+c.add_variable("mu1_pt_by_mass",			1)
+c.add_variable("mu2_pt_by_mass",			1)
 
-c.add_variable('muons.pt',					2)
+# c.add_variable('muons.pt',					2)
 
 c.add_spectator('muPairs.mass',				1)
 c.add_spectator('muPairs.phi',				1)
-# c.add_spectator('muons.pt',					2)
+c.add_spectator('muons.pt',					2)
 c.add_spectator('muons.eta',				2)
 c.add_spectator('muons.phi',				2)
 c.add_spectator('muons.isMediumID',			2)
