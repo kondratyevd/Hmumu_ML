80977a62c6709eadbf04a56cbcfd8c5fa5a8d949
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2018-10-16 16:57:08 -0400

    fix

--- packages/Keras/keras_models.py
+++ packages/Keras/keras_models.py
@@ -80,22 +80,38 @@ def GetListOfModels(nVar):
 	LAMBDA = 0.1 # lambda for penalty
 
 
-	def loss_kldiv(y_in,x_in):
-	    """
-	    mass sculpting penlaty term usking kullback_leibler_divergence
-	    y_in: truth [h, y]
-	    x: predicted NN output for y
-	    h: the truth mass histogram vector "one-hot encoded" (length NBINS=40)
-	    y: the truth categorical labels  "one-hot encoded" (length NClasses=2)
-	    """
+	# def loss_kldiv(y_in,x_in):
+	#     """
+	#     mass sculpting penlaty term usking kullback_leibler_divergence
+	#     y_in: truth [h, y]
+	#     x: predicted NN output for y
+	#     h: the truth mass histogram vector "one-hot encoded" (length NBINS=40)
+	#     y: the truth categorical labels  "one-hot encoded" (length NClasses=2)
+	#     """
+	#     h = y_in[:,0:NBINS]
+	#     y = y_in[:,NBINS:NBINS+2]
+	#     x = x_in[:,NBINS:NBINS+2]	    
+	#     h_blike_slike_s = K.dot(K.transpose(h), K.dot(tf.diag(y[:,0]),x))
+	#     h_blike_slike_b = K.dot(K.transpose(h), K.dot(tf.diag(y[:,1]),x))
+	#     h_blike_s = h_blike_slike_s[:,1]
+	#     h_blike_s = h_blike_s / K.sum(h_blike_s,axis=0)
+	#     h_slike_s = h_blike_slike_s[:,0]
+	#     h_slike_s = h_slike_s / K.sum(h_slike_s,axis=0)
+	#     h_blike_b = h_blike_slike_b[:,1]
+	#     h_blike_b = h_blike_b / K.sum(h_blike_b,axis=0)
+	#     h_slike_b = h_blike_slike_s[:,0]
+	#     h_slike_b = h_slike_b / K.sum(h_slike_b,axis=0)
+	
+	#     return categorical_crossentropy(y, x) + \
+	#         LAMBDA*kullback_leibler_divergence(h_blike_s, h_slike_s) + \
+	#         LAMBDA*kullback_leibler_divergence(h_blike_b, h_slike_b)  
+
+
+
+	def loss_kldiv1(y_in,x_in):
 	    h = y_in[:,0:NBINS]
 	    y = y_in[:,NBINS:NBINS+2]
 	    x = x_in[:,NBINS:NBINS+2]	    
-	    # h_all = K.dot(K.transpose(h), y)
-	    # h_all_q = h_all[:,0]
-	    # h_all_h = h_all[:,1]
-	    # h_all_q = h_all_q / K.sum(h_all_q,axis=0)
-	    # h_all_h = h_all_h / K.sum(h_all_h,axis=0)
 	    h_blike_slike_s = K.dot(K.transpose(h), K.dot(tf.diag(y[:,0]),x))
 	    h_blike_slike_b = K.dot(K.transpose(h), K.dot(tf.diag(y[:,1]),x))
 	    h_blike_s = h_blike_slike_s[:,1]
@@ -108,32 +124,11 @@ def GetListOfModels(nVar):
 	    h_slike_b = h_slike_b / K.sum(h_slike_b,axis=0)
 	
 	    return categorical_crossentropy(y, x) + \
-	        LAMBDA*kullback_leibler_divergence(h_blike_s, h_slike_s) + \
-	        LAMBDA*kullback_leibler_divergence(h_blike_b, h_slike_b)  
-
-
-
-	def loss_kldiv1(y_in,x_in):
-	    h = y_in[:,0:NBINS]			# truth mass histogram (batch, NBINS)
-	    y = y_in[:,NBINS:NBINS+2]	# truth labels (batch, 2)
-	    x = x_in[:,NBINS:NBINS+2]	# predicted labels (batch, 2)
-	    h_slike_blike_s = K.dot(K.transpose(h), K.dot(tf.diag(y[:,0]),x)) # predicted mas histograms for true signal training events (NBINS, 2)
-	    h_slike_blike_b = K.dot(K.transpose(h), K.dot(tf.diag(y[:,1]),x)) # predicted mas histograms for true bkg training events (NBINS, 2)
-	    h_slike_s = h_slike_blike_s[:,0]								  
-	    h_slike_s = h_slike_s / K.sum(h_slike_s,axis=0)	# mass PDF for true signal events identified as signal
-	    h_blike_s = h_slike_blike_s[:,1]
-	    h_blike_s = h_blike_s / K.sum(h_blike_s,axis=0) # mass PDF for true signal events identified as bkg
-	    h_slike_b = h_slike_blike_b[:,0]
-	    h_slike_b = h_slike_b / K.sum(h_slike_b,axis=0) # mass PDF for true bkg events identified as signal
-	    h_blike_b = h_slike_blike_s[:,1]
-	    h_blike_b = h_blike_b / K.sum(h_blike_b,axis=0) # mass PDF for true bkg events identified as bkg
-	
-	    return categorical_crossentropy(y, x) + \
 	        0.1*kullback_leibler_divergence(h_blike_s, h_slike_s) + \
-	        0.1*kullback_leibler_divergence(h_blike_b, h_slike_b)   
+	        0.1*kullback_leibler_divergence(h_blike_b, h_slike_b)    
 
 
-	model_50_D2_25_D2_kldiv1 = model_init('model_50_D2_25_D2_kldiv1', nVar, 2048, 100, [loss_kldiv], 'adam')
+	model_50_D2_25_D2_kldiv1 = model_init('model_50_D2_25_D2_kldiv1', nVar, 2048, 100, [loss_kldiv1], 'adam')
 	x = Dense(50, name = model_50_D2_25_D2_kldiv1.name+'_layer_1', activation='relu')(model_50_D2_25_D2_kldiv1.inputs)
 	x = Dropout(0.2)(x)
 	x = Dense(25, name = model_50_D2_25_D2_kldiv1.name+'_layer_2', activation='relu')(x)
