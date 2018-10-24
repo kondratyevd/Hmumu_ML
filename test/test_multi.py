504d0bfde93d13335e817541133bdbfb0588dfdd
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2018-10-24 15:23:16 -0400

    test

--- test/test_multi.py
+++ test/test_multi.py
@@ -116,9 +116,9 @@ c.add_package("Keras_multi")
 # c.add_method("UCSD_model")	# 50_D2
 # c.add_method("model_50_25") # no Dropout
 # c.add_method("model_50_D1_25_D1") # Dropout 0.1
-c.add_method("model_50_D2_25_D2") # Dropout 0.2
+# c.add_method("model_50_D2_25_D2") # Dropout 0.2
 
-# c.custom_loss = True
+c.custom_loss = True
 # c.add_method("model_50_D2_25_D2_25_D2_mass_control_bkg_0p5")
 # c.add_method("model_50_D2_25_D2_25_D2_mass_control_bkg_1")
 # c.add_method("model_50_D2_25_D2_25_D2_mass_control_bkg_1p5")
@@ -126,7 +126,7 @@ c.add_method("model_50_D2_25_D2") # Dropout 0.2
 # c.add_method("model_50_D2_25_D2_25_D2_mass_control_bkg_2p5")
 # c.add_method("model_50_D2_25_D2_25_D2_mass_control_bkg_3")
 # c.add_method("model_50_D2_25_D2_25_D2_mass_control_bkg_3p5")
-# c.add_method("model_50_D2_25_D2_25_D2_mass_control_bkg_4")
+c.add_method("model_50_D2_25_D2_25_D2_mass_control_bkg_4")
 
 # c.add_method("model_50_D2_25_D2_mutual_mass_control_5")
 # c.add_method("model_50_D2_25_D2_mutual_mass_control_sym_5")
