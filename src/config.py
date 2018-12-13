b0b784569f1824f9cba4df16450879cdb52a36ca
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2018-12-13 09:48:28 -0500

    Jet variable initialization: -5, except dEta (100)

--- src/config.py
+++ src/config.py
@@ -47,14 +47,14 @@ variables.append(Variable("nBMed"			,	"nBMed"				,"nBMed",  				"", 	  	'I', Fal
 
 variables.append(Variable("jets.eta"		,	"eta"				,"Jet #eta",  			"",   		'F', True, 		"nJets"		,	-5	, False	))
 variables.append(Variable("jets.phi"		,	"phi"				,"Jet #phi",  			"",   		'F', True, 		"nJets"		,	-5	, False	)) 
-variables.append(Variable("jetPairs.dEta"	,	"dEta"				,"jj |#delta#eta|",  	"",   		'F', True, 		"nJetPairs"	,	-5	, True	)) 
+variables.append(Variable("jetPairs.dEta"	,	"dEta"				,"jj |#delta#eta|",  	"",   		'F', True, 		"nJetPairs"	,	100	, True	)) 
 variables.append(Variable("jetPairs.mass"	,	"mass"				,"jj mass",  			"GeV",   	'F', True, 		"nJetPairs"	,	-5	, False	))
 
-variables.append(Variable("min_dR_mu_jet"	,	"min_dR_mu_jet"		,"min_dR_mu_jet", 		"",			'F', False, 	"nJets"	,	100	, False	))
-variables.append(Variable("max_dR_mu_jet"	,	"max_dR_mu_jet"		,"max_dR_mu_jet", 		"",			'F', False, 	"nJets"	,	100	, False	))
-variables.append(Variable("min_dR_mumu_jet"	,	"min_dR_mumu_jet"	,"min_dR_mumu_jet", 	"",			'F', False, 	"nJets"	,	100	, False	))
-variables.append(Variable("max_dR_mumu_jet"	,	"max_dR_mumu_jet"	,"max_dR_mumu_jet", 	"",			'F', False, 	"nJets"	,	100	, False	))
-variables.append(Variable("zepenfeld"		,	"zepenfeld"			,"zepenfeld", 			"",			'F', False, 	"nJetPairs"	,100	, False	))
+variables.append(Variable("min_dR_mu_jet"	,	"min_dR_mu_jet"		,"min_dR_mu_jet", 		"",			'F', False, 	"nJets"	,	-5	, False	))
+variables.append(Variable("max_dR_mu_jet"	,	"max_dR_mu_jet"		,"max_dR_mu_jet", 		"",			'F', False, 	"nJets"	,	-5	, False	))
+variables.append(Variable("min_dR_mumu_jet"	,	"min_dR_mumu_jet"	,"min_dR_mumu_jet", 	"",			'F', False, 	"nJets"	,	-5	, False	))
+variables.append(Variable("max_dR_mumu_jet"	,	"max_dR_mumu_jet"	,"max_dR_mumu_jet", 	"",			'F', False, 	"nJets"	,	-5	, False	))
+variables.append(Variable("zepenfeld"		,	"zepenfeld"			,"zepenfeld", 			"",			'F', False, 	"nJetPairs"	,-5	, False	))
 
 
 variables.append(Variable("mu1_pt_by_mass"	,	"mu1_pt_by_mass"	,"Muon1 p_{T} / Mmm", 	"",			'F', False, 	"nMuons"	,	0	, False	))
