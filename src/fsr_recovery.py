7ffb97687ae83e69ff023803eaed9a65b559ba9d
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-01-14 19:43:36 -0500

    Added photon isolation

--- src/fsr_recovery.py
+++ src/fsr_recovery.py
@@ -9,8 +9,8 @@ ROOT.gStyle.SetOptStat(0)
 sys.argv = oldargv
 
 # load FWLite C++ libraries
-ROOT.gSystem.Load("libFWCoreFWLite.so");
-ROOT.gSystem.Load("libDataFormatsFWLite.so");
+ROOT.gSystem.Load("libFWCoreFWLite.so")
+ROOT.gSystem.Load("libDataFormatsFWLite.so")
 ROOT.FWLiteEnabler.enable()
 
 # load FWlite python libraries
@@ -67,11 +67,27 @@ def mu2_selection(mu):
     passed = (mu.pt()>20) and (abs(mu.eta())<2.4)# and (mu_rel_iso(mu)<0.25)
     return passed
 
-def photon_preselection(photon, mu1, mu2):
+def photon_preselection(photon, mu1, mu2, pfCands):
     ph_mu1_dR = deltaR(photon.eta(), photon.phi(), mu1.eta(), mu1.phi())
     ph_mu2_dR = deltaR(photon.eta(), photon.phi(), mu2.eta(), mu2.phi())
-    passed = (photon.pt()>1) and (abs(photon.eta()<2.4)) and (photon.pdgId()==22) and (ph_mu1_dR<0.5 or ph_mu2_dR<0.5)
+    passed = (photon.pt()>1) and (abs(photon.eta()<2.4)) and (photon.pdgId()==22) and (ph_mu1_dR<0.5 or ph_mu2_dR<0.5) and (photonPfIso03(photon, pfCands)<1.8)
     return passed
+
+def photonPfIso03(pho, pfCands):
+    ptsum = 0.0
+    for i_pfc, pfc in enumerate(pfCands.product()):
+        dr = deltaR(pho.eta(), pho.phi(), pfc.eta(), pfc.phi())
+        if (dr >= 0.3)
+            continue
+        if (pfc.charge() != 0 and abs(pfc.pdgId()) == 211 and pfc.pt() > 0.2):
+            if (dr > 0.0001):
+                ptsum += pfc.pt()
+        elif (pfc.charge() == 0 and (abs(pfc.pdgId()) == 22 || abs(pfc.pdgId()) == 130) and pfc.pt() > 0.5):
+            if (dr > 0.01)
+                ptsum += pfc.pt()
+  return ptsum
+
+
 
 def loop_over_events(path):
     
@@ -83,7 +99,7 @@ def loop_over_events(path):
     mass_fsr_hist = ROOT.TH1D("mass_fsr", "", 40,110,150)
     mass_hist_tagged = ROOT.TH1D("mass_tagged", "", 40,110,150)
     mass_fsr_hist_tagged = ROOT.TH1D("mass_fsr_tagged", "", 40,110,150)
-    fsr_spectrum = ROOT.TH1D("fsr_spectrum", "", 100,0,50)
+    # fsr_spectrum = ROOT.TH1D("fsr_spectrum", "", 100,0,50)
 
     tree = ROOT.TTree("tree", "tree")
     tree.SetDirectory(0)
@@ -95,6 +111,7 @@ def loop_over_events(path):
     max_abs_eta_mu = array("f", [0])
     mu1_eta = array("f", [0])
     mu2_eta = array("f", [0])
+    fsr_spectrum = array("f", [0])
 
     tree.Branch('mass', mass, 'mass/F')
     tree.Branch('fsr_tag', fsr_tag, 'fsr_tag/I')
@@ -103,6 +120,7 @@ def loop_over_events(path):
     tree.Branch('max_abs_eta_mu', max_abs_eta_mu, 'max_abs_eta_mu/F')
     tree.Branch('mu1_eta', mu1_eta, 'mu1_eta/F')    
     tree.Branch('mu2_eta', mu2_eta, 'mu2_eta/F')
+    tree.Branch('fsr_spectrum', fsr_spectrum, 'fsr_spectrum/F')
 
     for filename in os.listdir(path):
         if filename.endswith("C.root"): 
@@ -114,6 +132,7 @@ def loop_over_events(path):
                 fsr_2tag[0] = -999
                 mass_postFSR[0] = -999
                 max_abs_eta_mu[0] = -999
+                fsr_spectrum[0] = -999
                 event.getByLabel(jetLabel, jets)
                 event.getByLabel(muonLabel, muons)
                 event.getByLabel(pfCandsLabel, pfCands)
@@ -121,8 +140,8 @@ def loop_over_events(path):
                 if (iev % 1000) is 0: 
                     print "Event # %i"%iev
 
-                if iev>10000:
-                    break
+                # if iev>10000:
+                #     break
             
                 mu1 = None
                 mu2 = None
@@ -151,68 +170,69 @@ def loop_over_events(path):
                         if abs(mu.eta())>max_abs_eta_mu[0]:
                             max_abs_eta_mu[0] = abs(mu.eta())
 
-                if mu1 and mu2:
+                if (not mu1) or (not mu2):
+                    continue
 
-                    # preselect photons: cuts on pT, eta, id, and close to at least one muon
-                    for i_pfc, pfc in enumerate(pfCands.product()):
-                        if photon_preselection(pfc, mu1, mu2):
-                            preselected_photons.append(pfc)
+                # preselect photons: cuts on pT, eta, id, and close to at least one muon
+                for i_pfc, pfc in enumerate(pfCands.product()):
+                    if photon_preselection(pfc, mu1, mu2, pfCands):
+                        preselected_photons.append(pfc)
     
-                    min_dR1_over_et2 = 0.012
-                    min_dR2_over_et2 = 0.012
+                min_dR1_over_et2 = 0.012
+                min_dR2_over_et2 = 0.012
     
-                    # find closest photon to the first muon
-                    for pfc in preselected_photons:
-                        ph_mu1_dR = deltaR(pfc.eta(), pfc.phi(), mu1.eta(), mu1.phi())
-                        if (ph_mu1_dR<0.5) and (ph_mu1_dR/(pfc.et()*pfc.et()) < min_dR1_over_et2):
-                            min_dR1_over_et2 = ph_mu1_dR/(pfc.et()*pfc.et())
-                            photon1 = pfc  
+                # find closest photon to the first muon
+                for pfc in preselected_photons:
+                    ph_mu1_dR = deltaR(pfc.eta(), pfc.phi(), mu1.eta(), mu1.phi())
+                    if (ph_mu1_dR<0.5) and (ph_mu1_dR/(pfc.et()*pfc.et()) < min_dR1_over_et2):
+                        min_dR1_over_et2 = ph_mu1_dR/(pfc.et()*pfc.et())
+                        photon1 = pfc  
     
-                    # find closest photon to the second muon
-                    for pfc in preselected_photons:
-                        ph_mu2_dR = deltaR(pfc.eta(), pfc.phi(), mu1.eta(), mu1.phi())
-                        if (ph_mu2_dR<0.5) and (ph_mu2_dR/(pfc.et()*pfc.et()) < min_dR2_over_et2):
-                            min_dR2_over_et2 = ph_mu2_dR/(pfc.et()*pfc.et())
-                            if photon1:
-                                if deltaR(pfc.eta(), pfc.phi(), photon1.eta(), photon1.phi())>0.001:
-                                    photon2 = pfc 
-                            else:
-                                photon2 = pfc    
-    
-                    if photon1:
-                        mu1_p4 = mu1.p4()+photon1.p4()
-                    else:
-                        mu1_p4 = mu1.p4()
-    
-                    if photon2:
-                        mu2_p4 = mu2.p4()+photon2.p4()
-                    else:
-                        mu2_p4 = mu2.p4()                    
-                                
-                    if isolated(mu1, mu2, photon1, photon2):
-                        dimu_mass = (mu1.p4() + mu2.p4()).M()
-                        dimu_fsr_mass = (mu1_p4+mu2_p4).M()
-                        mass[0] = dimu_mass
-                        mass_postFSR[0] = dimu_fsr_mass
-                        mass_fsr_hist.Fill(dimu_fsr_mass)
-                        mass_hist.Fill(dimu_mass)
-                        if photon1 or photon2:
-                            mass_fsr_hist_tagged.Fill(dimu_fsr_mass)
-                            mass_hist_tagged.Fill(dimu_mass)
-                            fsr_tag[0] = 1
-                        else:
-                            fsr_tag[0] = 0
-
-                        if photon1 and photon2:
-                            fsr_2tag[0] = 1
-                        else:
-                            fsr_2tag[0] = 0
-
+                # find closest photon to the second muon
+                for pfc in preselected_photons:
+                    ph_mu2_dR = deltaR(pfc.eta(), pfc.phi(), mu1.eta(), mu1.phi())
+                    if (ph_mu2_dR<0.5) and (ph_mu2_dR/(pfc.et()*pfc.et()) < min_dR2_over_et2):
+                        min_dR2_over_et2 = ph_mu2_dR/(pfc.et()*pfc.et())
                         if photon1:
-                            fsr_spectrum.Fill(photon1.pt())
-                        if photon2:
-                            fsr_spectrum.Fill(photon2.pt())
-                        tree.Fill()
+                            if deltaR(pfc.eta(), pfc.phi(), photon1.eta(), photon1.phi())>0.001:
+                                photon2 = pfc 
+                        else:
+                            photon2 = pfc    
+    
+                if photon1:
+                    mu1_p4 = mu1.p4()+photon1.p4()
+                else:
+                    mu1_p4 = mu1.p4()
+    
+                if photon2:
+                    mu2_p4 = mu2.p4()+photon2.p4()
+                else:
+                    mu2_p4 = mu2.p4()                    
+                            
+                if isolated(mu1, mu2, photon1, photon2):
+                    dimu_mass = (mu1.p4() + mu2.p4()).M()
+                    dimu_fsr_mass = (mu1_p4+mu2_p4).M()
+                    mass[0] = dimu_mass
+                    mass_postFSR[0] = dimu_fsr_mass
+                    mass_fsr_hist.Fill(dimu_fsr_mass)
+                    mass_hist.Fill(dimu_mass)
+                    if photon1 or photon2:
+                        mass_fsr_hist_tagged.Fill(dimu_fsr_mass)
+                        mass_hist_tagged.Fill(dimu_mass)
+                        fsr_tag[0] = 1
+                    else:
+                        fsr_tag[0] = 0
+
+                    if photon1 and photon2:
+                        fsr_2tag[0] = 1
+                    else:
+                        fsr_2tag[0] = 0
+
+                    if photon1:
+                        fsr_spectrum[0] = photon1.pt()
+                    elif photon2:
+                        fsr_spectrum[0] = photon2.pt()
+                    tree.Fill()
 
     mass_hist_tagged.SetLineColor(ROOT.kBlue)
     mass_fsr_hist_tagged.SetLineColor(ROOT.kRed)
@@ -223,7 +243,7 @@ def loop_over_events(path):
     tree.Write()
     out_file.Close()
 
-    return mass_hist, mass_fsr_hist, mass_hist_tagged, mass_fsr_hist_tagged, fsr_spectrum
+    return mass_hist, mass_fsr_hist, mass_hist_tagged, mass_fsr_hist_tagged
 
 def set_out_path(path):
     try:
@@ -248,7 +268,7 @@ ggh_path = "/mnt/hadoop/store/mc/RunIIFall17MiniAODv2/GluGluHToMuMu_M125_13TeV_a
 out_path = "plots/fsr_recovery/"
 set_out_path(out_path)
 
-mass_hist, mass_fsr_hist, mass_hist_tagged, mass_fsr_hist_tagged, fsr_spectrum =  loop_over_events(ggh_path)
+mass_hist, mass_fsr_hist, mass_hist_tagged, mass_fsr_hist_tagged =  loop_over_events(ggh_path)
 
 legend = ROOT.TLegend(0.65,0.7,0.89,0.89)
 legend.AddEntry(mass_hist, 'pre-FSR', 'l')
@@ -257,9 +277,5 @@ legend.AddEntry(mass_fsr_hist, ' post-FSR', 'l')
 plot_hists([mass_fsr_hist, mass_hist], "inclusive_removeFromIso", out_path, legend)
 plot_hists([mass_fsr_hist_tagged, mass_hist_tagged], "fsr_tagged_removeFromIso", out_path, legend)
 
-canvas = ROOT.TCanvas("sp", "sp", 800, 800)
-canvas.cd()
-fsr_spectrum.Draw("histsame")
-canvas.SaveAs(out_path+"fsr_spectrum.png")
 
 
