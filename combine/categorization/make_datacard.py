63418044460473d795be232fbf796052e9a58019
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-01-28 17:45:37 -0500

    Added 1 and 24 categories

--- combine/categorization/make_datacard.py
+++ combine/categorization/make_datacard.py
@@ -182,7 +182,8 @@ def create_datacard(bins, path, name, workspace_filename):
     out_file.close()
 
 
-bins_list = [0, 0.8, 1.7, 2.4]
-create_datacard(bins_list, "combine/categorization/", "datacard", "workspace")
-
+# bins_list = [0, 0.8, 1.7, 2.4]
+# create_datacard(bins_list, "combine/categorization/", "datacard", "workspace")
+create_datacard([0, 2.4], "combine/categorization/", "datacard_1cat", "workspace_1cat")
+create_datacard([0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4], "combine/categorization/", "datacard_24cat", "workspace_24cat")
 
