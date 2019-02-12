433f790511f705d7fecb4bb3ba481cdd8677a0cc
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-02-12 13:39:35 -0500

    Add nuisance parameters to the signal model

--- combine/categorization/make_datacard.py
+++ combine/categorization/make_datacard.py
@@ -92,6 +92,90 @@ def add_sig_model(w, cat_number, input_path, cut):
     Import(w, smodel)
     return signal_rate
 
+def add_sig_model_with_nuisances(w, cat_number, input_path, cut):
+    var = w.var("mass")
+    var.setBins(5000)
+    max_abs_eta_var = ROOT.RooRealVar("max_abs_eta_mu","Max abs(eta) of muons", 0, 2.4) 
+    mu1_eta = ROOT.RooRealVar("mu1_eta","mu1_eta", -2.4, 2.4) 
+    mu2_eta = ROOT.RooRealVar("mu2_eta","mu2_eta", -2.4, 2.4) 
+    signal_tree = ROOT.TChain("tree_H2Mu_gg")
+    # signal_tree.Add(input_path+"/output_test.root")  
+    signal_tree.Add(input_path+"/output_train.root")  
+    signal_hist_name = "signal_%i"%cat_number
+    signal_hist = ROOT.TH1D(signal_hist_name, signal_hist_name, 40, 110, 150)
+    dummy = ROOT.TCanvas("dummy", "dummy", 800, 800)
+    dummy.cd()
+    # signal_tree.Draw("mass>>%s"%(signal_hist_name), "(%s)*weight*5"%(cut)) # only 20% of events were saved in "test" file, hence the weight
+    signal_tree.Draw("mass>>%s"%(signal_hist_name), "(%s)*weight*5/4"%(cut)) # only 80% of events were saved in "train" file, hence the weight
+    dummy.Close()
+    signal_rate = signal_hist.Integral()
+    print signal_rate
+
+    w.factory("cat%i_mixGG [0.5, 0.0, 1.0]"%cat_number)
+    w.factory("cat%i_mixGG1 [0.5, 0.0, 1.0]"%cat_number)
+
+    # w.factory("cat%i_mu_res_beta [0, 0, 0]"%cat_number)
+    # w.factory("cat%i_mu_scale_beta [0, 0, 0]"%cat_number)
+
+    w.factory("mu_res_beta [0, 0, 0]")
+    w.factory("mu_scale_beta [0, 0, 0]")
+
+    w.factory("cat%i_mu_res_unc [0.1, 0.1, 0.1]"%cat_number)
+    w.factory("cat%i_mu_scale_unc [0.0005, 0.0005, 0.0005]"%cat_number)
+
+    w.var("cat%i_mu_res_unc"%cat_number).setConstant(True)
+    w.var("cat%i_mu_scale_unc"%cat_number).setConstant(True)
+
+    mixGG = w.var("cat%i_mixGG"%cat_number)
+    mixGG1 = w.var("cat%i_mixGG1"%cat_number)
+
+    # w.factory("EXPR::cat%i_mean1_times_nuis('cat%i_mean1*(1 + cat%i_mu_scale_unc*cat%i_mu_scale_beta)',{cat%i_mean1[125.0, 120., 130.],cat%i_mu_scale_unc,cat%i_mu_scale_beta})"%(cat_number,cat_number,cat_number,cat_number,cat_number,cat_number,cat_number))
+    # w.factory("EXPR::cat%i_mean2_times_nuis('cat%i_mean2*(1 + cat%i_mu_scale_unc*cat%i_mu_scale_beta)',{cat%i_mean2[125.0, 120., 130.],cat%i_mu_scale_unc,cat%i_mu_scale_beta})"%(cat_number,cat_number,cat_number,cat_number,cat_number,cat_number,cat_number))
+    # w.factory("EXPR::cat%i_mean3_times_nuis('cat%i_mean3*(1 + cat%i_mu_scale_unc*cat%i_mu_scale_beta)',{cat%i_mean3[125.0, 120., 130.],cat%i_mu_scale_unc,cat%i_mu_scale_beta})"%(cat_number,cat_number,cat_number,cat_number,cat_number,cat_number,cat_number))
+
+    w.factory("EXPR::cat%i_mean1_times_nuis('cat%i_mean1*(1 + cat%i_mu_scale_unc*mu_scale_beta)',{cat%i_mean1[125.0, 120., 130.],cat%i_mu_scale_unc,mu_scale_beta})"%(cat_number,cat_number,cat_number,cat_number,cat_number))
+    w.factory("EXPR::cat%i_mean2_times_nuis('cat%i_mean2*(1 + cat%i_mu_scale_unc*mu_scale_beta)',{cat%i_mean2[125.0, 120., 130.],cat%i_mu_scale_unc,mu_scale_beta})"%(cat_number,cat_number,cat_number,cat_number,cat_number))
+    w.factory("EXPR::cat%i_mean3_times_nuis('cat%i_mean3*(1 + cat%i_mu_scale_unc*mu_scale_beta)',{cat%i_mean3[125.0, 120., 130.],cat%i_mu_scale_unc,mu_scale_beta})"%(cat_number,cat_number,cat_number,cat_number,cat_number))
+
+    w.factory("expr::cat%i_deltaM21('cat%i_mean2-cat%i_mean1',{cat%i_mean2, cat%i_mean1})"%(cat_number,cat_number,cat_number,cat_number,cat_number))
+    w.factory("expr::cat%i_deltaM31('cat%i_mean3-cat%i_mean1',{cat%i_mean3, cat%i_mean1})"%(cat_number,cat_number,cat_number,cat_number,cat_number))
+
+    # w.factory("EXPR::cat%i_mean2_final('cat%i_mean2_times_nuis + cat%i_mu_res_unc*cat%i_mu_res_beta*cat%i_deltaM21',{cat%i_mean2_times_nuis, cat%i_mu_res_unc, cat%i_mu_res_beta, cat%i_deltaM21})"%(cat_number,cat_number,cat_number,cat_number,cat_number,cat_number,cat_number,cat_number,cat_number))
+    # w.factory("EXPR::cat%i_mean3_final('cat%i_mean3_times_nuis + cat%i_mu_res_unc*cat%i_mu_res_beta*cat%i_deltaM31',{cat%i_mean3_times_nuis, cat%i_mu_res_unc, cat%i_mu_res_beta, cat%i_deltaM31})"%(cat_number,cat_number,cat_number,cat_number,cat_number,cat_number,cat_number,cat_number,cat_number))
+
+    # w.factory("EXPR::cat%i_width1_times_nuis('cat%i_width1*(1 + cat%i_mu_res_unc*cat%i_mu_res_beta)',{cat%i_width1[1.0, 0.5, 5.0],cat%i_mu_res_unc,cat%i_mu_res_beta})"%(cat_number,cat_number,cat_number,cat_number,cat_number,cat_number,cat_number))
+    # w.factory("EXPR::cat%i_width2_times_nuis('cat%i_width2*(1 + cat%i_mu_res_unc*cat%i_mu_res_beta)',{cat%i_width2[5.0, 2.0, 10.],cat%i_mu_res_unc,cat%i_mu_res_beta})"%(cat_number,cat_number,cat_number,cat_number,cat_number,cat_number,cat_number))
+    # w.factory("EXPR::cat%i_width3_times_nuis('cat%i_width3*(1 + cat%i_mu_res_unc*cat%i_mu_res_beta)',{cat%i_width3[5.0, 1.0, 10.],cat%i_mu_res_unc,cat%i_mu_res_beta})"%(cat_number,cat_number,cat_number,cat_number,cat_number,cat_number,cat_number))
+
+    w.factory("EXPR::cat%i_mean2_final('cat%i_mean2_times_nuis + cat%i_mu_res_unc*mu_res_beta*cat%i_deltaM21',{cat%i_mean2_times_nuis, cat%i_mu_res_unc, mu_res_beta, cat%i_deltaM21})"%(cat_number,cat_number,cat_number,cat_number,cat_number,cat_number,cat_number))
+    w.factory("EXPR::cat%i_mean3_final('cat%i_mean3_times_nuis + cat%i_mu_res_unc*mu_res_beta*cat%i_deltaM31',{cat%i_mean3_times_nuis, cat%i_mu_res_unc, mu_res_beta, cat%i_deltaM31})"%(cat_number,cat_number,cat_number,cat_number,cat_number,cat_number,cat_number))
+
+    w.factory("EXPR::cat%i_width1_times_nuis('cat%i_width1*(1 + cat%i_mu_res_unc*mu_res_beta)',{cat%i_width1[1.0, 0.5, 5.0],cat%i_mu_res_unc, mu_res_beta})"%(cat_number,cat_number,cat_number,cat_number,cat_number))
+    w.factory("EXPR::cat%i_width2_times_nuis('cat%i_width2*(1 + cat%i_mu_res_unc*mu_res_beta)',{cat%i_width2[5.0, 2.0, 10.],cat%i_mu_res_unc, mu_res_beta})"%(cat_number,cat_number,cat_number,cat_number,cat_number))
+    w.factory("EXPR::cat%i_width3_times_nuis('cat%i_width3*(1 + cat%i_mu_res_unc*mu_res_beta)',{cat%i_width3[5.0, 1.0, 10.],cat%i_mu_res_unc, mu_res_beta})"%(cat_number,cat_number,cat_number,cat_number,cat_number))
+
+    w.factory("Gaussian::cat%i_gaus1(mass, cat%i_mean1_times_nuis, cat%i_width1_times_nuis)"%(cat_number, cat_number, cat_number))
+    w.factory("Gaussian::cat%i_gaus2(mass, cat%i_mean2_final, cat%i_width2_times_nuis)"%(cat_number, cat_number, cat_number))
+    w.factory("Gaussian::cat%i_gaus3(mass, cat%i_mean3_final, cat%i_width3_times_nuis)"%(cat_number, cat_number, cat_number))
+    gaus1 = w.pdf('cat%i_gaus1'%(cat_number))
+    gaus2 = w.pdf('cat%i_gaus2'%(cat_number))
+    gaus3 = w.pdf('cat%i_gaus3'%(cat_number))
+    gaus12 = ROOT.RooAddPdf('cat%i_gaus12'%(cat_number), 'cat%i_gaus12'%(cat_number), gaus1, gaus2, mixGG)
+    smodel = ROOT.RooAddPdf('cat%i_ggh'%cat_number, 'cat%i_ggh'%cat_number, gaus3, gaus12, mixGG1)
+    # Import(w,smodel)
+    w.Print()
+    signal_ds = ROOT.RooDataSet("signal_ds","signal_ds", signal_tree, ROOT.RooArgSet(var, max_abs_eta_var, mu1_eta, mu2_eta), cut)
+    res = smodel.fitTo(signal_ds, ROOT.RooFit.Range("full"),ROOT.RooFit.Save(), ROOT.RooFit.Verbose(False))
+    res.Print()
+    sigParamList = ["mean1", "mean2", "mean3", "width1", "width2", "width3", "mixGG", "mixGG1"]
+    for par in sigParamList:
+        par_var = w.var("cat%s_%s"%(cat_number,par))
+        par_var.setConstant(True)
+    w.var("mu_res_beta").setRange(-5, 5)
+    w.var("mu_scale_beta").setRange(-5, 5)
+    Import(w, smodel)
+    return signal_rate
+
 def add_bkg_model(w, cat_number, input_path, cut):
     data = add_data(w, cat_number, input_path, cut)
     var = w.var("mass")
@@ -119,7 +203,7 @@ def add_bkg_model(w, cat_number, input_path, cut):
     return bkg_rate
 
 
-def make_eta_categories(bins, output_path, filename, statUnc=False):
+def make_eta_categories(bins, output_path, filename, statUnc=False, nuis=False):
     input_path = "output/Run_2018-12-19_14-25-02/Keras_multi/model_50_D2_25_D2_25_D2/root/"     # no index
     # input_path = "output/Run_2019-01-18_14-34-07/Keras_multi/model_50_D2_25_D2_25_D2/root/"      # index '1'
     nCat = len(bins)-1
@@ -146,7 +230,12 @@ def make_eta_categories(bins, output_path, filename, statUnc=False):
         cat_names.append(name)
 
         cut = "((max_abs_eta_mu>%.5f)&(max_abs_eta_mu<%.5f))"%(eta_min, eta_max)
-        sig_rate = add_sig_model(w, i, input_path, cut) 
+        
+        if nuis:
+            sig_rate = add_sig_model_with_nuisances(w, i, input_path, cut)    
+        else:
+            sig_rate = add_sig_model(w, i, input_path, cut) 
+
         bkg_rate = add_bkg_model(w, i, input_path, cut)
 
         combine_import = combine_import+"shapes cat%i_bkg  cat%i %s.root w:cat%i_bkg\n"%(i,i,filename,i)
@@ -177,13 +266,13 @@ def make_eta_categories(bins, output_path, filename, statUnc=False):
     return combine_import, combine_bins+"\n"+combine_obs+"\n", combine_bins_str+combine_proc_str+combine_ipro_str+combine_rate_str, combine_unc+"\n"
 
 
-def create_datacard(bins, path, name, workspace_filename, statUnc=False): 
+def create_datacard(bins, path, name, workspace_filename, statUnc=False, nuis=False): 
     try:
         os.makedirs(path)
     except OSError as e:
         if e.errno != errno.EEXIST:
             raise
-    import_str, bins_obs, cat_strings, unc_str = make_eta_categories(bins, path, workspace_filename, statUnc=statUnc)
+    import_str, bins_obs, cat_strings, unc_str = make_eta_categories(bins, path, workspace_filename, statUnc=statUnc, nuis=nuis)
     out_file = open(path+name+".txt", "w")
     out_file.write("imax *\n")
     out_file.write("jmax *\n")
@@ -197,6 +286,9 @@ def create_datacard(bins, path, name, workspace_filename, statUnc=False):
     out_file.write("------------------------------\n")
     if statUnc:
         out_file.write(unc_str)
+    if nuis:
+        out_file.write("mu_res_beta    param    0    1.\n")
+        out_file.write("mu_scale_beta    param    0    1.\n")
     out_file.close()
 
 def make_BOE_categories(barrel_cut, endcap_cut, output_path, filename):
@@ -352,9 +444,11 @@ def create_BOE_datacard(barrel_cut, endcap_cut, output_path, name, workspace_fil
 #     # print bins
 
 
+# create_datacard([0, 2.4], "combine/categorization/nuis_test/", "datacard", "workspace", nuis=True)
+
 for i in range(23):
     bins = [0, (i+1)/10.0, 2.4]
-    create_datacard(bins, "combine/categorization/2cat_scan_5000bins/", "datacard_2cat_%i_5000bins"%(i+1), "workspace_2cat_%i_5000bins"%(i+1), statUnc=False)
+    create_datacard(bins, "combine/categorization/2cat_scan_5000bins_nuis/", "datacard_2cat_%i"%(i+1), "workspace_2cat_%i"%(i+1), nuis=True)
 
 
 # for i in range(14):
