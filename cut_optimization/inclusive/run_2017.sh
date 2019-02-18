d749e4d7fecea5cfc0b71c26dd07877ade2130e0
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-02-18 13:17:10 -0500

    Added significance calculations for 2017

---
+++ cut_optimization/inclusive/run_2017.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+CURRENT_LOCATION=$(pwd)
+SIG_INPUT_PATH="/home/dkondra/Hmumu_analysis/Hmumu_ML/combine/fsr_test_2017_full.root"
+DATA_INPUT_PATH="/mnt/hadoop/store/user/dkondrat/skim/2016/SingleMu_2016/*root"
+SIG_TREE="tree"
+DATA_TREE="dimuons/tree"
+LUMI=35866
+OUTPUT_PATH=output/
+python categorize.py --sig_in_path "$SIG_INPUT_PATH" --data_in_path "$DATA_INPUT_PATH" --out_path "$OUTPUT_PATH" --sig_tree "$SIG_TREE" --data_tree "$DATA_TREE" --lumi "$LUMI"
+cd $OUTPUT_PATH
+for filename in *.txt; do
+    # this is to retrieve whatever there is between "datacard" and ".txt" and use as a suffix for combine output. 
+    # I stole that from some stackexchange topic, don't really know how it works
+    SUFF=$(echo "$filename" | sed "s|datacard\(.*\)\.txt|\1|");
+    echo $filename
+    combine -M Significance --expectSignal=1 -t -1 -n "$SUFF"_2017 -d $filename
+done
+cd $CURRENT_LOCATION
