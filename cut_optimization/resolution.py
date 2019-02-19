ea677c996cb7b1fcf7f8fc2e50c114767b989b5b
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-02-19 16:08:23 -0500

    Add ZH resolution

--- cut_optimization/resolution.py
+++ cut_optimization/resolution.py
@@ -141,10 +141,12 @@ def make_resolution_plot(sources, label):
 sig_sources = []
 
 sig_sources.append(SignalSrc("ggH_2016", "ggH 2016", "/mnt/hadoop/store/user/dkondrat/skim/2016/H2Mu_gg/*root", "dimuons/tree", "1", ROOT.kBlue))
-sig_sources.append(SignalSrc("ggH_2017", "ggH 2017", "~/Hmumu_analysis/Hmumu_ML/combine/fsr_test_2017_full.root", "tree", "1", ROOT.kRed))
+sig_sources.append(SignalSrc("ggH_2017", "ggH 2017", "~/Hmumu_analysis/Hmumu_ML/cut_optimization/miniaod_skim/ggH_2017.root", "tree", "1", ROOT.kRed))
 sig_sources.append(SignalSrc("ggH_2018", "ggH 2018", "~/Hmumu_analysis/Hmumu_ML/combine/fsr_test_2018_full.root", "tree", "1", ROOT.kGreen+1))
 
 sig_sources.append(SignalSrc("vbf_2016", "VBF 2016", "/mnt/hadoop/store/user/dkondrat/skim/2016/H2Mu_VBF/*root", "dimuons/tree", "1", ROOT.kViolet+1))
+
+sig_sources.append(SignalSrc("zh_2017", "ZH 2017", "~/Hmumu_analysis/Hmumu_ML/cut_optimization/miniaod_skim/zh_2017.root", "tree", "1", ROOT.kYellow))
 
 
 # sig_sources.append(SignalSrc("ggH_local_1", "ggH local 1", "/Users/dmitrykondratyev/root_files/mc/2017/updated/gluglu/gluglu_1.root", "dimuons/tree", "weight_over_lumi*(mu1_eta<0)", ROOT.kRed))
