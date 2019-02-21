98b24ac90d0d1b5cb84fcf2484b01cf7d2a8618d
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-02-21 13:00:40 -0500

    test

--- cut_optimization/skim_miniaod.py
+++ cut_optimization/skim_miniaod.py
@@ -199,17 +199,17 @@ set_out_path(output_path)
 
 # ggH 2017 ##
 
-ggh_2017 = "/mnt/hadoop/store/mc/RunIIFall17MiniAODv2/GluGluHToMuMu_M125_13TeV_amcatnloFXFX_pythia8/MINIAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/20000/"
-loop_over_events(ggh_2017, output_path+"ggH_2017.root")
-write_weights_to_tree(output_path+"ggH_2017.root", lumi=0.009618) #ggH
+# ggh_2017 = "/mnt/hadoop/store/mc/RunIIFall17MiniAODv2/GluGluHToMuMu_M125_13TeV_amcatnloFXFX_pythia8/MINIAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/20000/"
+# loop_over_events(ggh_2017, output_path+"ggH_2017.root")
+# write_weights_to_tree(output_path+"ggH_2017.root", lumi=0.009618) #ggH
 
 # ggH 2018 #
 
-# ggh_2018_1 = "/mnt/hadoop/store/mc/RunIIAutumn18MiniAOD/GluGluHToMuMu_M125_TuneCP5_PSweights_13TeV_amcatnloFXFX_pythia8/MINIAODSIM/102X_upgrade2018_realistic_v15-v2/100000/"
+ggh_2018_1 = "/mnt/hadoop/store/mc/RunIIAutumn18MiniAOD/GluGluHToMuMu_M125_TuneCP5_PSweights_13TeV_amcatnloFXFX_pythia8/MINIAODSIM/102X_upgrade2018_realistic_v15-v2/100000/"
 # ggh_2018_2 = "/mnt/hadoop/store/mc/RunIIAutumn18MiniAOD/GluGluHToMuMu_M125_TuneCP5_PSweights_13TeV_amcatnloFXFX_pythia8/MINIAODSIM/102X_upgrade2018_realistic_v15-v2/110000/"
 # ggh_2018_3 = "/mnt/hadoop/store/mc/RunIIAutumn18MiniAOD/GluGluHToMuMu_M125_TuneCP5_PSweights_13TeV_amcatnloFXFX_pythia8/MINIAODSIM/102X_upgrade2018_realistic_v15-v2/120000/"
 # ggh_2018_4 = "/mnt/hadoop/store/mc/RunIIAutumn18MiniAOD/GluGluHToMuMu_M125_TuneCP5_PSweights_13TeV_amcatnloFXFX_pythia8/MINIAODSIM/102X_upgrade2018_realistic_v15-v2/80000/"
-# loop_over_events(ggh_2018_1, output_path+"ggh_2018_1.root")
+loop_over_events(ggh_2018_1, output_path+"ggh_2018_1.root")
 # loop_over_events(ggh_2018_2, output_path+"ggh_2018_2.root")
 # loop_over_events(ggh_2018_3, output_path+"ggh_2018_3.root")
 # loop_over_events(ggh_2018_4, output_path+"ggh_2018_4.root")
