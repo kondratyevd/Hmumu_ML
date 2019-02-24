e0f5110b98219474cccf0cc585ddf50d5b2ad615
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-02-24 16:59:58 -0500

    Update plotter

--- cut_optimization/2cat_scan/plot_significance.py
+++ cut_optimization/2cat_scan/plot_significance.py
@@ -22,10 +22,15 @@ parser.add_argument('-p', action='store', dest='process', default="ggh", help='P
 args = parser.parse_args()
 
 
-plot_2016 = SignPlot("plot_2016", "%s 2016 w/ nuis."%args.process, "/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/%s_2016_nuis/"%args.process, ROOT.kBlue)
-plot_2017 = SignPlot("plot_2017", "%s 2017 w/ nuis."%args.process, "/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/%s_2017_nuis/"%args.process, ROOT.kRed)
-plot_2018 = SignPlot("plot_2018", "%s 2018 w/ nuis."%args.process, "/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/%s_2018_nuis/"%args.process, ROOT.kGreen)
+plot_2016 = SignPlot("plot_2016", "%s 2016"%args.process, "/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/%s_2016/"%args.process, ROOT.kBlue)
+plot_2017 = SignPlot("plot_2017", "%s 2017"%args.process, "/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/%s_2017/"%args.process, ROOT.kRed)
+plot_2018 = SignPlot("plot_2018", "%s 2018"%args.process, "/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/%s_2018/"%args.process, ROOT.kGreen)
 plots = [plot_2016, plot_2017, plot_2018]
+
+plot_2016_nuis = SignPlot("plot_2016", "%s 2016 w/ nuis."%args.process, "/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/%s_2016_nuis/"%args.process, ROOT.kBlue)
+plot_2017_nuis = SignPlot("plot_2017", "%s 2017 w/ nuis."%args.process, "/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/%s_2017_nuis/"%args.process, ROOT.kRed)
+plot_2018_nuis = SignPlot("plot_2018", "%s 2018 w/ nuis."%args.process, "/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/%s_2018_nuis/"%args.process, ROOT.kGreen)
+plots_nuis = [plot_2016_nuis, plot_2017_nuis, plot_2018_nuis]
 
 canvas = ROOT.TCanvas("c", "c", 800, 800)
 canvas.cd()
@@ -43,6 +48,17 @@ for ip, p in enumerate(plots):
     else:
         p.graph.Draw("plsame")
     legend.AddEntry(p.graph, p.title, "pl")
+
+for ip, p in enumerate(plots_nuis):
+    for i in range(1, 24):
+        tree = ROOT.TChain("limit")
+        tree.Add(p.path+"higgsCombine_2cat_%i.Significance.mH120.root"%i)
+        for iev,  event in enumerate(tree):
+            p.graph.SetPoint(i-1, i/10.0, event.limit)
+        p.graph.SetLineStyle(2)
+        p.graph.Draw("plsame")
+    legend.AddEntry(p.graph, p.title, "pl")
+
 legend.Draw()
-canvas.Print("/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/significance_%s_nuis.png"%args.process)
-canvas.SaveAs("/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/significance_%s_nuis.root"%args.process)
+canvas.Print("/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/significance_%s.png"%args.process)
+canvas.SaveAs("/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/significance_%s.root"%args.process)
