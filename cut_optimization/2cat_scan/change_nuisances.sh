af4c991b08822f5c97dd38bffed03d5c6e4162de
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-02-26 10:07:20 -0500

    Add significance calculation

--- cut_optimization/2cat_scan/change_nuisances.sh
+++ cut_optimization/2cat_scan/change_nuisances.sh
@@ -4,11 +4,15 @@ CURRENT_LOCATION=$(pwd)
 SOURCE_DIR=/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/ggh_2017_nuis/
 TARGET_DIR=/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/ggh_2017_nuis_0p01/
 mkdir $TARGET_DIR
-cp $SOURCE_DIR/* $TARGET_DIR
+cp $SOURCE_DIR/datacard* $TARGET_DIR
+cp $SOURCE_DIR/workspace* $TARGET_DIR
 
 cd $TARGET_DIR
 for filename in *.txt; do
 	sed -i 's/param    0    1./param    0    0.1/g' $filename
+	SUFF=$(echo "$filename" | sed "s|datacard\(.*\)\.txt|\1|");
+    echo $filename
+    combine -M Significance --expectSignal=1 -t -1 -n "$SUFF" -d $filename
 done
 
 cd $CURRENT_LOCATION
