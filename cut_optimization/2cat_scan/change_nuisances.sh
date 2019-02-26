1ef3253752d7fe96871fbf74678b8fbb00a0e0bf
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-02-26 10:03:46 -0500

    Script to change nuisances without reproducing the datacards

---
+++ cut_optimization/2cat_scan/change_nuisances.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+CURRENT_LOCATION=$(pwd)
+SOURCE_DIR=/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/ggh_2017_nuis/
+TARGET_DIR=/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/ggh_2017_nuis_0p01/
+mkdir $TARGET_DIR
+cp $SOURCE_DIR/* $TARGET_DIR
+
+cd $TARGET_DIR
+for filename in *.txt; do
+	sed -i 's/param    0    1./param    0    0.1/g' filename.txt
+done
+
+cd $CURRENT_LOCATION
