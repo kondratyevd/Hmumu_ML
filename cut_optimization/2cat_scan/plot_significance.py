775305c47e2d1dafd7995b94f6cd42423c7de9c4
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-02-27 14:31:16 -0500

    plot different nuis values

--- cut_optimization/2cat_scan/plot_significance.py
+++ cut_optimization/2cat_scan/plot_significance.py
@@ -27,22 +27,27 @@ plot_2017 = SignPlot("plot_2017", "%s 2017"%args.process, "/home/dkondra/Hmumu_a
 plot_2017_psweights = SignPlot("plot_2017_psweights", "%s 2017 PSweights"%args.process, "/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/%s_2017_psweights/"%args.process, ROOT.kOrange)
 plot_2018 = SignPlot("plot_2018", "%s 2018"%args.process, "/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/%s_2018/"%args.process, ROOT.kGreen)
 # plot_2018_test = SignPlot("plot_2018_test", "%s 2018 (250k evts)"%args.process, "/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/%s_2018_test/"%args.process, ROOT.kBlack)
-plots = [plot_2016, plot_2017, plot_2017_psweights, plot_2018]
+plots = [plot_2016, plot_2017_psweights, plot_2018]
 # plots = [plot_2017]
 
-plot_2016_nuis = SignPlot("plot_2016", "%s 2016 w/ nuis."%args.process, "/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/%s_2016_nuis/"%args.process, ROOT.kBlue)
-plot_2017_nuis = SignPlot("plot_2017", "%s 2017 w/ nuis."%args.process, "/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/%s_2017_nuis/"%args.process, ROOT.kRed)
+# plot_2016_nuis = SignPlot("plot_2016", "%s 2016 w/ nuis."%args.process, "/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/%s_2016_nuis/"%args.process, ROOT.kBlue)
+# plot_2017_nuis = SignPlot("plot_2017", "%s 2017 w/ nuis."%args.process, "/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/%s_2017_nuis/"%args.process, ROOT.kRed)
 # plot_2018_nuis = SignPlot("plot_2018", "%s 2018 w/ nuis."%args.process, "/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/%s_2018_nuis/"%args.process, ROOT.kGreen)
 # plot_2018_test_nuis = SignPlot("plot_2018_test_nuis", "%s 2018 w/ nuis. (250k evts)"%args.process, "/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/%s_2018_test_nuis/"%args.process, ROOT.kBlack)
 
-# plot_2017_nuis_0p01 = SignPlot("plot_2017_psweights_0p01", "%s 2017 1%% nuis."%args.process, "/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/%s_2017_psweights_nuis_0p01/"%args.process, ROOT.kOrange)
-# plot_2017_nuis_0p01 = SignPlot("plot_2017", "%s 2017 w/ 0.1%% nuis."%args.process, "/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/%s_2017_nuis_0p001/"%args.process, ROOT.kOrange)
+plot_2017_nuis_0p01 = SignPlot("plot_2017_psweights_0p01", "%s 2017 1%% nuis."%args.process, "/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/%s_2017_psweights_nuis_0p01/"%args.process, ROOT.kOrange)
+plot_2017_nuis_0p02 = SignPlot("plot_2017_psweights_0p02", "%s 2017 2%% nuis."%args.process, "/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/%s_2017_psweights_nuis_0p02/"%args.process, ROOT.kOrange)
+plot_2017_nuis_0p05 = SignPlot("plot_2017_psweights_0p05", "%s 2017 5%% nuis."%args.process, "/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/%s_2017_psweights_nuis_0p05/"%args.process, ROOT.kOrange)
+plot_2017_nuis_0p1 = SignPlot("plot_2017_psweights_0p1", "%s 2017 10%% nuis."%args.process, "/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/%s_2017_psweights_nuis_0p1/"%args.process, ROOT.kOrange)
 
-# plots_nuis = [plot_2016_nuis, plot_2017_nuis, plot_2017_nuis_0p01]
-
+plot_2017_nuis_0p01.SetMarkerStyle(21)
+plot_2017_nuis_0p02.SetMarkerStyle(22)
+plot_2017_nuis_0p05.SetMarkerStyle(23)
+plot_2017_nuis_0p1.SetMarkerStyle(24)
+plots_nuis = [plot_2017_nuis_0p01, plot_2017_nuis_0p02, plot_2017_nuis_0p05, plot_2017_nuis_0p1]
 # plots_nuis = [plot_2017_nuis_0p1, plot_2017_nuis_0p01]
 
-plots_nuis = []
+# plots_nuis = []
 canvas = ROOT.TCanvas("c", "c", 800, 800)
 canvas.cd()
 
@@ -71,10 +76,10 @@ for ip, p in enumerate(plots_nuis):
         for iev,  event in enumerate(tree):
             p.graph.SetPoint(i-1, i/10.0, event.limit)
         p.graph.SetLineStyle(2)
-        p.graph.SetMarkerStyle(21)
+        # p.graph.SetMarkerStyle(21)
         p.graph.Draw("plsame")
     legend.AddEntry(p.graph, p.title, "pl")
 
 legend.Draw()
-canvas.Print("/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/significance_%s_psweights.png"%args.process)
-canvas.SaveAs("/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/significance_%s_psweights.root"%args.process)
+canvas.Print("/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/significance_%s_nuis_test.png"%args.process)
+canvas.SaveAs("/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/significance_%s_nuis_test.root"%args.process)
