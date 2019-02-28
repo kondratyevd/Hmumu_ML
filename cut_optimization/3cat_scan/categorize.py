9bc7c2198db6e0651e279e9040e58e5bc070990c
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-02-28 15:51:31 -0500

    scan options for 3 categories

--- cut_optimization/3cat_scan/categorize.py
+++ cut_optimization/3cat_scan/categorize.py
@@ -4,12 +4,19 @@ from make_datacards import create_datacard
 import argparse
 
 parser = argparse.ArgumentParser(description='')
-parser.add_argument('--in_path', action='store', dest='input_path', help='Input path')
+parser.add_argument('--sig_in_path', action='store', dest='sig_input_path', help='Input path')
+parser.add_argument('--data_in_path', action='store', dest='data_input_path', help='Input path')
+parser.add_argument('--sig_tree', action='store', dest='sig_tree', help='Tree name')
+parser.add_argument('--data_tree', action='store', dest='data_tree', help='Tree name')
 parser.add_argument('--out_path', action='store', dest='output_path', help='Output path')
+parser.add_argument('--lumi', action='store', dest='lumi', help='Integrated luminosity')
+parser.add_argument('--nuis', action='store_true', dest='nuis', help='Nuisances')
+parser.add_argument('--nuis_val', action='store', dest='nuis_val', help='Resolution uncertainty')
+args = parser.parse_args()
 
 args = parser.parse_args()
 
 for i in range(24):
     for j in range(i):
         bins = [0.0, (j+1)/10.0, (i+1)/10.0, 2.4]
-        create_datacard(bins, args.input_path, args.output_path, "datacard_3cat_%i_%i"%(j+1, i+1), "workspace_3cat_%i_%i"%(j+1, i+1), nuis=False)
+        create_datacard(bins, args.sig_input_path, args.sig_tree, args.data_input_path, args.data_tree, args.output_path, "datacard_3cat_%i_%i"%(j+1, i+1), "workspace_3cat_%i_%i"%(j+1, i+1), args.lumi, nuis=args.nuis, nuis_val=args.nuis_val)
--- cut_optimization/3cat_scan/run.sh
+++ cut_optimization/3cat_scan/run.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
 CURRENT_LOCATION=$(pwd)
-INPUT_PATH=../../output/Run_2018-12-19_14-25-02/Keras_multi/model_50_D2_25_D2_25_D2/root/
-OUTPUT_PATH=output/
-python categorize.py --in_path $INPUT_PATH --out_path $OUTPUT_PATH
+SIG_INPUT_PATH="/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/miniaod_skim/ggh_2017_psweights.root"
+DATA_INPUT_PATH="/mnt/hadoop/store/user/dkondrat/skim/2016/SingleMu_2016/*root"
+SIG_TREE="tree"
+DATA_TREE="dimuons/tree"
+LUMI=35866
+OUTPUT_PATH=output/ggh_2017_psweights/
+mkdir $OUTPUT_PATH
+python categorize.py --nuis --nuis_val 0.1 --sig_in_path "$SIG_INPUT_PATH" --data_in_path "$DATA_INPUT_PATH" --out_path "$OUTPUT_PATH" --sig_tree "$SIG_TREE" --data_tree "$DATA_TREE" --lumi "$LUMI"
 cd $OUTPUT_PATH
 for filename in *.txt; do
     # this is to retrieve whatever there is between "datacard" and ".txt" and use as a suffix for combine output. 
-    # I stole that from some stacexchange topic, don't really know how it works
+    # I stole that from some stackexchange topic, don't really know how it works
     SUFF=$(echo "$filename" | sed "s|datacard\(.*\)\.txt|\1|");
     echo $filename
     combine -M Significance --expectSignal=1 -t -1 -n "$SUFF" -d $filename
