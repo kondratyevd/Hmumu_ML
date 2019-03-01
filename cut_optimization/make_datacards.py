b5849bb89973c72867f1415465953a9a891c78e6
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-03-01 17:44:35 -0500

    fix

--- cut_optimization/make_datacards.py
+++ cut_optimization/make_datacards.py
@@ -172,7 +172,7 @@ def add_sig_model_dcb(w, cat_number, input_path, sig_tree, lumi, cut):
     signal_rate = signal_hist.Integral()
 
     # ROOT.gROOT.ProcessLine(".L /home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/RooDCBShape.cxx")
-    gSystem.Load("/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/RooDCBShape_cxx.so")
+    ROOT.gSystem.Load("/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/RooDCBShape_cxx.so")
     w.factory("RooDCBShape::cat%i_ggh(mass, cat%i_mean[125,120,130], cat%i_sigma[2,0,5], cat%i_alphaL[2,0,25] , cat%i_alphaR[2,0,25], cat%i_nL[1.5,0,25], cat%i_nR[1.5,0,25])"%(cat_number,cat_number,cat_number,cat_number,cat_number,cat_number,cat_number))
     smodel = w.pdf("cat%i_ggh"%cat_number)
     w.Print()
