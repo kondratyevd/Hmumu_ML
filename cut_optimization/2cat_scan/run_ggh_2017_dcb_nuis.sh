5042a29c7a28fc525ab197483e40fd4acfa033c4
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-03-02 12:48:16 -0500

    DCB w/ nuisances

--- cut_optimization/2cat_scan/run_ggh_2017_dcb_nuis.sh
+++ cut_optimization/2cat_scan/run_ggh_2017_dcb_nuis.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+root -l -q load.C
 CURRENT_LOCATION=$(pwd)
 SIG_INPUT_PATH="/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/miniaod_skim/ggh_2017_psweights.root"
 DATA_INPUT_PATH="/mnt/hadoop/store/user/dkondrat/skim/2016/SingleMu_2016/*root"
@@ -11,10 +11,10 @@ mkdir $OUTPUT_PATH
 python categorize.py --nuis --nuis_val 0.1 --smodel dcb --sig_in_path "$SIG_INPUT_PATH" --data_in_path "$DATA_INPUT_PATH" --out_path "$OUTPUT_PATH" --sig_tree "$SIG_TREE" --data_tree "$DATA_TREE" --lumi "$LUMI"
 cd $OUTPUT_PATH
 for filename in *.txt; do
-    # this is to retrieve whatever there is between "datacard" and ".txt" and use as a suffix for combine output. 
+    # this is to retrieve whatever there is between "datacard" and ".txt" and use as a suffix for combine output.
     # I stole that from some stackexchange topic, don't really know how it works
     SUFF=$(echo "$filename" | sed "s|datacard\(.*\)\.txt|\1|");
     echo $filename
-    combine -M Significance --expectSignal=1 -t -1 -n "$SUFF" -d $filename
+    combine -M Significance --expectSignal=1 -t -1 -n "$SUFF" -d $filename --LoadLibrary /home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/RooDCBShape_cxx.so
 done
 cd $CURRENT_LOCATION
