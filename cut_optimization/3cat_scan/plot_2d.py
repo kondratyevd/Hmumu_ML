8f1d5bbe3778a284e8bc974a0f6841ba5171a51d
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-03-02 14:49:31 -0500

    Plot output

---
+++ cut_optimization/3cat_scan/plot_2d.py
@@ -0,0 +1,24 @@
+import ROOT
+
+ROOT.gStyle.SetOptStat(0)
+
+label = ggh_2017_psweights
+filedir = 'output/%s/'%label
+
+hist2d = ROOT.TH2D("eta_categories", "", 23, 0.05, 2.35, 23, 0.05, 2.35)
+
+for i in range(23):
+    for j in range(i):
+#        print j+1, i+1
+        combine_filename = "higgsCombine_3cat_%i_%i_.Significance.mH120.root"%(j+1, i+1)
+        tree = ROOT.TChain("limit")
+        tree.Add(combine_filename)
+        for event in tree:
+            significance = event.limit
+       hist2d.Fill((j+1)/10.0, (i+1)/10.0, significance)
+
+canv = ROOT.TCanvas("c", "c", 800, 800)
+canv.cd()
+hist2d.Draw("colz")
+canv.Print("significance_%s.png"%label)
+canv.SaveAs("significance_%s.root"%label)        
