def491aecded304aa9e47f749b6e8b9d4f12cb64
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-03-05 13:01:09 -0500

    update

--- cut_optimization/2cat_scan/plot_significance.py
+++ cut_optimization/2cat_scan/plot_significance.py
@@ -43,7 +43,7 @@ plot_2017_nuis_0p1 = SignPlot("plot_2017_psweights_0p1", "%s 2017 3Gaus 10%% nui
 plot_2017_nuis_0 = SignPlot("plot_2017_psweights_0", "%s 2017 only scale nuis."%args.process, "/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/%s_2017_psweights_nuis_0/"%args.process, ROOT.kOrange)
 plot_2017_nuis_onlyinnermost = SignPlot("plot_2017_psweights_onlyinnermost", "%s 2017 resolution nuis.\nonly for innermost Gaussian"%args.process, "/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/%s_2017_psweights_onlyinnermost/"%args.process, ROOT.kOrange)
 plot_2017_dcb_nuis = SignPlot("plot_2017_dcb_nuis", "%s 2017 DCB 10%% nuis."%args.process, "/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/%s_2017_dcb_nuis/"%args.process, ROOT.kRed)
-plot_2017_dcb_nuis_0 = SignPlot("plot_2017_dcb_nuis", "%s 2017 DCB only scale nuis."%args.process, "/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/%s_2017_psweights_nuis_0_dcb/"%args.process, ROOT.kRed)
+plot_2017_dcb_nuis_0 = SignPlot("plot_2017_dcb_nuis_0", "%s 2017 DCB only scale nuis."%args.process, "/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/%s_2017_psweights_nuis_0_dcb/"%args.process, ROOT.kRed)
 # plot_2017_nuis_0p01.graph.SetMarkerStyle(21)
 # plot_2017_nuis_0p02.graph.SetMarkerStyle(22)
 # plot_2017_nuis_0p05.graph.SetMarkerStyle(23)
