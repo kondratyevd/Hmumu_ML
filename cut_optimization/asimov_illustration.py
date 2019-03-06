dbdc2af7f2331d4c90ccc7e37ccc4e42662c1988
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-03-05 23:27:28 -0500

    fix

--- cut_optimization/asimov_illustration.py
+++ cut_optimization/asimov_illustration.py
@@ -11,21 +11,21 @@ def plot_initial_shapes(eta_min, eta_max):
 
     sig_tree = ROOT.TChain(sig_tree_name)
     sig_tree.Add(signal_input)
-    sig_hist = ROOT.TH1D("signal", "", 100, 110, 135)
+    sig_hist = ROOT.TH1D("signal", "", 20, 110, 135)
     sig_tree.Draw("mass>>signal", cuts)
     sig_hist.Scale(1/sig_hist.Integral())
 
     data_tree = ROOT.TChain(data_tree_name)
     data_tree.Add(data_input)
-    data_hist = ROOT.TH1D("data", "", 100, 110, 135)    
+    data_hist = ROOT.TH1D("data", "", 20, 110, 135)    
     data_tree.Draw("mass>>data", cuts)
     data_hist.Scale(1/data_hist.Integral())
 
     canvas = ROOT.TCanvas("c", "c", 800, 800)
     canvas.cd()
 
-    data_hist.Draw('hist')
-    sig_hist.Draw('histsame')
+    sig_hist.Draw('hist')
+    data_hist.Draw('histsame')
 
     canvas.SaveAs('plots/asimov/initial_shapes.png')
 
