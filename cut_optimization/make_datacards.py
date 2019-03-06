8d6d00e78313c3ef4a130e4b1982d75a132a5af7
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-03-06 01:41:24 -0500

    fix

--- cut_optimization/make_datacards.py
+++ cut_optimization/make_datacards.py
@@ -289,12 +289,12 @@ def make_eta_categories(bins, sig_input_path, sig_tree, data_input_path, data_tr
         print "Applying cut: ", cut
         if '3gaus' in smodel:
             if nuis:
-                sig_rate = add_sig_model_with_nuisances(w, i, sig_input_path, sig_tree, lumi, cut, res_unc_val)    
+                sig_rate = add_sig_model_with_nuisances(w, i, sig_input_path, sig_tree, lumi, cut, res_unc_val, scale_unc_val)    
             else:
                 sig_rate = add_sig_model(w, i, sig_input_path, sig_tree, lumi, cut) 
         elif 'dcb' in smodel:
             if nuis:
-                sig_rate = add_sig_model_dcb_with_nuisances(w, i, sig_input_path, sig_tree, lumi, cut, res_unc_val)    
+                sig_rate = add_sig_model_dcb_with_nuisances(w, i, sig_input_path, sig_tree, lumi, cut, res_unc_val, scale_unc_val)    
             else:
                 sig_rate = add_sig_model_dcb(w, i, sig_input_path, sig_tree, lumi, cut) 
 
