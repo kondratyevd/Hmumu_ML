53a70d742cf56c0e8bc484d828169d1c3579273a
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-03-06 00:38:55 -0500

    update

--- cut_optimization/asimov_illustration.py
+++ cut_optimization/asimov_illustration.py
@@ -28,7 +28,7 @@ def add_sig_model_3gaus(w, cat_number, input_path, sig_tree, cut):
     signal_tree.Add(input_path)  
     print "Loaded tree from "+input_path+" with %i entries."%signal_tree.GetEntries()    
     signal_hist_name = "signal_%i"%cat_number
-    signal_hist = ROOT.TH1D(signal_hist_name, signal_hist_name, 40, 110, 135)
+    signal_hist = ROOT.TH1D(signal_hist_name, signal_hist_name, 100, 110, 135)
     dummy = ROOT.TCanvas("dummy", "dummy", 800, 800)
     dummy.cd()
     signal_tree.Draw("mass>>%s"%(signal_hist_name), cut)
@@ -72,7 +72,7 @@ def add_sig_model_dcb(w, cat_number, input_path, sig_tree, cut):
     signal_tree.Add(input_path)  
     print "Loaded tree from "+input_path+" with %i entries."%signal_tree.GetEntries()    
     signal_hist_name = "signal_%i"%cat_number
-    signal_hist = ROOT.TH1D(signal_hist_name, signal_hist_name, 40, 110, 150)
+    signal_hist = ROOT.TH1D(signal_hist_name, signal_hist_name, 100, 110, 150)
     dummy = ROOT.TCanvas("dummy", "dummy", 800, 800)
     dummy.cd()
     signal_tree.Draw("mass>>%s"%(signal_hist_name), cut)
@@ -103,7 +103,7 @@ def add_data(w, cat_number, input_path, data_tree, cut):
     data_tree.Add(input_path)  
     print "Loaded tree from "+input_path+" with %i entries."%data_tree.GetEntries()
     data_hist_name = "data_%i"%cat_number
-    data_hist = ROOT.TH1D(data_hist_name, data_hist_name, 40, 110, 135)
+    data_hist = ROOT.TH1D(data_hist_name, data_hist_name, 100, 110, 135)
     dummy = ROOT.TCanvas("dummy", "dummy", 800, 800)
     dummy.cd()
     data_tree.Draw("mass>>%s"%(data_hist_name), cut)
@@ -158,8 +158,8 @@ def plot_initial_shapes(eta_min, eta_max):
     data_tree.Draw("mass>>data_initial_full", cuts)  
     data_tree.Draw("mass>>data_initial", "(mass<120||mass>130)&%s"%cuts)
     data_hist.Sumw2()
-    # data_hist.Scale(1/data_hist_full.Integral())
-    data_hist.Scale(1/data_hist.Integral())
+    data_hist.Scale(1/data_hist_full.Integral())
+    # data_hist.Scale(1/data_hist.Integral())
     data_hist.SetLineWidth(3)
     data_hist.SetMarkerStyle(20)
     data_hist.SetMarkerSize(1)
