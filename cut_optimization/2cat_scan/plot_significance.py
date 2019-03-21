68a97799570bed30f8c1d76078b17c8fe0529f24
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-03-21 09:14:01 -0400

    fix

--- cut_optimization/2cat_scan/plot_significance.py
+++ cut_optimization/2cat_scan/plot_significance.py
@@ -29,7 +29,8 @@ plot_2017_dcb = SignPlot("plot_2017_dcb", "%s DCB"%args.process, "/home/dkondra/
 plot_2018 = SignPlot("plot_2018", "%s 3Gaus 2018"%args.process, "/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/%s_2018/"%args.process, ROOT.kGreen)
 # plot_2018_test = SignPlot("plot_2018_test", "%s 2018 (250k evts)"%args.process, "/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/%s_2018_test/"%args.process, ROOT.kBlack)
 # plots = [plot_2016, plot_2017_psweights, plot_2018]
-plots = [plot_2017_psweights, plot_2017_dcb, plot_2016, plot_2018]
+# plots = [plot_2017_psweights, plot_2017_dcb, plot_2016, plot_2018]
+plots = [plot_2017_psweights, plot_2017_dcb]
 # plots = [plot_2017_psweights]
 # plot_2016_nuis = SignPlot("plot_2016", "%s 2016 w/ nuis."%args.process, "/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/%s_2016_nuis/"%args.process, ROOT.kBlue)
 # plot_2017_nuis = SignPlot("plot_2017", "%s 2017 w/ nuis."%args.process, "/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/output/%s_2017_nuis/"%args.process, ROOT.kRed)
@@ -61,8 +62,8 @@ plot_2017_onlyres_dcb.graph.SetMarkerStyle(22)
 # plots_nuis = [plot_2017_nuis_0p01, plot_2017_nuis_0p02, plot_2017_nuis_0p05, plot_2017_nuis_0p1]
 # plots_nuis = [plot_2017_nuis_0p1, plot_2017_nuis_onlyinnermost, plot_2017_dcb_nuis, plot_2017_dcb_nuis_0, plot_2017_nuis_0]
 # plots_nuis = [plot_2017_nuis_0p1, plot_2017_dcb_nuis, plot_2017_dcb_nuis_0, plot_2017_nuis_0]
-plots_nuis = [plot_2017_nuis_0, plot_2017_onlyres, plot_2017_nuis_0p1, plot_2017_dcb_nuis_0, plot_2017_onlyres_dcb, plot_2017_dcb_nuis, plot_2017_nuis_onlyinnermost]
-
+# plots_nuis = [plot_2017_nuis_0, plot_2017_onlyres, plot_2017_nuis_0p1, plot_2017_dcb_nuis_0, plot_2017_onlyres_dcb, plot_2017_dcb_nuis, plot_2017_nuis_onlyinnermost]
+plots_nuis = [plot_2017_nuis_0p1, plot_2017_nuis_0p1_new, plot_2017_dcb_nuis]
 # plots_nuis = []
 canvas = ROOT.TCanvas("c", "c", 800, 800)
 canvas.cd()
@@ -97,5 +98,5 @@ for ip, p in enumerate(plots_nuis):
     legend.AddEntry(p.graph, p.title, "pl")
 
 legend.Draw()
-canvas.Print("/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/significance_%s_nuis_test_3.png"%args.process)
-canvas.SaveAs("/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/significance_%s_nuis_test_3.root"%args.process)
+canvas.Print("/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/significance_%s_nuis_test_4.png"%args.process)
+canvas.SaveAs("/home/dkondra/Hmumu_analysis/Hmumu_ML/cut_optimization/2cat_scan/significance_%s_nuis_test_4.root"%args.process)
