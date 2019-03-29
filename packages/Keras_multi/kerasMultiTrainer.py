3112822ce236a5e49734e1db3883a846be6a02c7
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-03-29 10:48:58 -0400

    run on full data

--- packages/Keras_multi/kerasMultiTrainer.py
+++ packages/Keras_multi/kerasMultiTrainer.py
@@ -55,7 +55,7 @@ class KerasMultiTrainer(object):
     def convert_to_pandas(self):
         for file in self.framework.files + self.framework.data_files:
             for filename in os.listdir(file.path):
-                if filename.endswith("_1.root"): 
+                if filename.endswith(".root"): 
                     with uproot.open(file.path+filename) as f: 
                         uproot_tree = f[self.framework.treePath]
                         single_file_df = pandas.DataFrame()
