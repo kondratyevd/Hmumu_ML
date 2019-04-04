fb0f60772fd2c281c4cbe5d9a0790c2af3142637
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-04-03 20:46:50 -0400

    apply old BDT

--- test/apply_test.py
+++ test/apply_test.py
@@ -21,9 +21,9 @@ gInterpreter.ProcessLine('#include "interface/GenJetInfo.h"')
 c = Framework()
 treePath = 'dimuons/tree'
 
-# c.add_data_to_evaluate('SingleMu2017B', '/mnt/hadoop/store/user/dkondrat/SingleMuon/SingleMu_2017B/190327_172730/0000/*root')
+c.add_data_to_evaluate('SingleMu2017B', '/mnt/hadoop/store/user/dkondrat/SingleMuon/SingleMu_2017B/190327_172730/0000/*root')
 
-# c.add_mc_to_evaluate('ZJets_AMC', '/mnt/hadoop/store/user/dkondrat//DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/ZJets_AMC/190323_195010/0000/*root', 5765.4)
+c.add_mc_to_evaluate('ZJets_AMC', '/mnt/hadoop/store/user/dkondrat//DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/ZJets_AMC/190323_195010/0000/*root', 5765.4)
 c.add_mc_to_evaluate('tt_ll_POW', '/mnt/hadoop/store/user/dkondrat/TTTo2L2Nu_TuneCP5_PSweights_13TeV-powheg-pythia8/tt_ll_POW/190322_195819/0000/*root', 85.656)
 c.add_mc_to_evaluate('H2Mu_gg', '/mnt/hadoop/store/user/dkondrat/GluGluHToMuMu_M-125_TuneCP5_PSweights_13TeV_powheg_pythia8/H2Mu_gg_powheg/190323_193526/0000/*root', 0.009618)
 c.add_mc_to_evaluate('H2Mu_VBF', '/mnt/hadoop/store/user/dkondrat/VBFHToMuMu_M-125_TuneCP5_PSweights_13TeV_powheg_pythia8/H2Mu_VBF_powheg/190322_195711/0000/*root', 0.0008208)
@@ -49,11 +49,11 @@ c.weigh_by_event(True)
 
 c.add_package("TMVA")
 
-# c.add_method("BDTG_UF_v1")
-# c.setApplication(outPath="/home/dkondra/tmp/BDTG_UF_v1/", xmlPath="/home/dkondra/Hmumu_analysis/Hmumu_ML/dataset/weights/BDTG_UF_v1/TMVAClassification_BDTG_UF_v1.weights.xml")
+c.add_method("BDTG_UF_v1")
+c.setApplication(outPath="/home/dkondra/tmp/BDTG_UF_v1/", xmlPath="/home/dkondra/Hmumu_analysis/Hmumu_ML/dataset/weights/BDTG_UF_v1/TMVAClassification_BDTG_UF_v1.weights.xml")
 
-c.add_method("BDTG_MIT")
-c.setApplication(outPath="/home/dkondra/tmp/BDTG_MIT/", xmlPath="/home/dkondra/Hmumu_analysis/Hmumu_ML/dataset/weights/BDTG_MIT/TMVAClassification_BDTG_MIT.weights.xml")
+# c.add_method("BDTG_MIT")
+# c.setApplication(outPath="/home/dkondra/tmp/BDTG_MIT/", xmlPath="/home/dkondra/Hmumu_analysis/Hmumu_ML/dataset/weights/BDTG_MIT/TMVAClassification_BDTG_MIT.weights.xml")
 
 
 c.apply_methods()
