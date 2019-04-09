094d24c4490d2d9e71ce343ca5c32bd866f04548
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-04-09 15:32:11 -0400

    fix

--- cut_optimization/full_categoriation/run.sh
+++ cut_optimization/full_categoriation/run.sh
@@ -74,7 +74,7 @@ DATA_TREE="tree_Data"
 # LUMI=35866
 OUTPUT_PATH=output/test_$1/
 mkdir $OUTPUT_PATH
-python categorize.py --option $1 --smodel '3gaus' --sig_in_path "$SIG_INPUT_PATH" --data_in_path "$DATA_INPUT_PATH" --out_path "$OUTPUT_PATH"  --data_tree "$DATA_TREE" --binary
+python categorize.py --option $1 --smodel '3gaus' --sig_in_path "$SIG_INPUT_PATH" --data_in_path "$DATA_INPUT_PATH" --out_path "$OUTPUT_PATH"  --data_tree "$DATA_TREE" --binary binary
 cd $OUTPUT_PATH
 for filename in *.txt; do
     # this is to retrieve whatever there is between "datacard" and ".txt" and use as a suffix for combine output. 
