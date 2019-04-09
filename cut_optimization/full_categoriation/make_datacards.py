578cccc1162133a905b67f5980a268407c2f543a
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-04-09 18:49:19 -0400

    test

--- cut_optimization/full_categoriation/make_datacards.py
+++ cut_optimization/full_categoriation/make_datacards.py
@@ -470,6 +470,8 @@ def make_dnn_categories(categories, sig_input_path, data_input_path, data_tree, 
 
 def create_datacard(categories, sig_in_path, data_in_path, data_tree, out_path, datacard_name, workspace_filename, statUnc=False, nuis=False, res_unc_val=0.1, scale_unc_val=0.0005, smodel='3gaus', method=""): 
     print "method:", method
+    if "BDT" in method:
+        print "!"*100
     print "="*30
     print "Categories: "
     for key, value in categories.iteritems():
