48aee24d3a747ead14d2efe2cec8cf67a36637ff
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-04-17 15:04:55 -0400

    test

--- src/calibrate_ebe.py
+++ src/calibrate_ebe.py
@@ -166,12 +166,16 @@ hist_ratio = {
 # tree_MC.Add(input_path_MC)
 # print "Loaded MC tree from "+input_path_MC+" with %i entries."%tree_MC.GetEntries() 
 
+tree_MC.SetDirectory(0)
+
 tree_Data = ROOT.TChain("dimuons/tree")
 tree_Data.Add(input_path_DataB)
 tree_Data.Add(input_path_DataC)
 tree_Data.Add(input_path_DataD)
 tree_Data.Add(input_path_DataE)
-# tree_Data.Add(input_path_DataF)
+tree_Data.Add(input_path_DataF)
+
+tree_Data.SetDirectory(0)
 
 print "Loaded data tree with %i entries."%tree_Data.GetEntries() 
 
