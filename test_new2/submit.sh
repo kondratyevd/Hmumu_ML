d5ec2d05ed68e597ee629434f18150d47c51d2fc
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-04-20 15:50:27 -0400

    fix

--- test_new2/submit.sh
+++ test_new2/submit.sh
@@ -8,19 +8,19 @@ case $1 in
 	# 	echo 'Running option 0'
 	# 	python test_new/test_dnn.py
 	# 	;;
-	1.1)
+	2.1)
 		echo 'Running option 2.1'
 		python test_new2/test_dnn_hiStat_m110To150.py
 		;;
-	1.2)
+	2.2)
 		echo 'Running option 2.2'
 		python test_new2/test_dnn_hiStat_m110To150_CS.py
 		;;
-	1.3)
+	2.3)
 		echo 'Running option 2.3'
 		python test_new2/test_dnn_hiStat_m120To130.py
 		;;
-	1.4)
+	2.4)
 		echo 'Running option 2.4'
 		python test_new2/test_dnn_hiStat_m120To130_CS.py
 		;;
