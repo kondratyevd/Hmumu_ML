d0b3eeb824723a542c0b5a3c49034383d9876388
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-04-20 14:37:46 -0400

    fix

--- test_new1/samples.py
+++ test_new1/samples.py
@@ -14,18 +14,18 @@ class DataSample(object):
 
 # Samples with added CS variables
 
-ggH_2017_powheg = SmpNtuple("H2Mu_gg","H2Mu_gg 2017 powheg", "/mnt/hadoop/store/user/dkondrat/GluGluHToMuMu_M-125_TuneCP5_PSweights_13TeV_powheg_pythia8/H2Mu_gg_powheg/190419_161651/0000/", False, 0.009618  ,1 )
-VBF_2017_powheg = SmpNtuple("H2Mu_VBF","H2Mu_VBF 2017 powheg", "/mnt/hadoop/store/user/dkondrat/VBFHToMuMu_M-125_TuneCP5_PSweights_13TeV_powheg_pythia8/H2Mu_VBF_powheg/190419_161716/0000/", False, 0.0008208 ,1  )
+ggH_2017_powheg = MCSample("H2Mu_gg","H2Mu_gg 2017 powheg", "/mnt/hadoop/store/user/dkondrat/GluGluHToMuMu_M-125_TuneCP5_PSweights_13TeV_powheg_pythia8/H2Mu_gg_powheg/190419_161651/0000/", 0.009618 )
+VBF_2017_powheg = MCSample("H2Mu_VBF","H2Mu_VBF 2017 powheg", "/mnt/hadoop/store/user/dkondrat/VBFHToMuMu_M-125_TuneCP5_PSweights_13TeV_powheg_pythia8/H2Mu_VBF_powheg/190419_161716/0000/", 0.0008208 )
 
-ZJets_aMC_2017_hiStat = SmpNtuple("ZJets_aMC", "ZJets_aMC 2017 high-stat", "/mnt/hadoop/store/user/dkondrat/DYJetsToLL_M-105To160_TuneCP5_PSweights_13TeV-amcatnloFXFX-pythia8/ZJets_AMC_hiStat/190419_161740/0000/", False, 47.17, 1)
+ZJets_aMC_2017_hiStat = MCSample("ZJets_aMC", "ZJets_aMC 2017 high-stat", "/mnt/hadoop/store/user/dkondrat/DYJetsToLL_M-105To160_TuneCP5_PSweights_13TeV-amcatnloFXFX-pythia8/ZJets_AMC_hiStat/190419_161740/0000/", 47.17)
 
-tt_ll_POW_2017 = SmpNtuple("tt_ll_POW","tt_ll 2017 powheg", "/mnt/hadoop/store/user/dkondrat/TTTo2L2Nu_TuneCP5_PSweights_13TeV-powheg-pythia8/tt_ll_POW/190419_161808/0000/", False, 85.656, 1)
+tt_ll_POW_2017 = MCSample("tt_ll_POW","tt_ll 2017 powheg", "/mnt/hadoop/store/user/dkondrat/TTTo2L2Nu_TuneCP5_PSweights_13TeV-powheg-pythia8/tt_ll_POW/190419_161808/0000/", 85.656)
 
-SingleMu2017B = SmpNtuple("SingleMu_2017B","SingleMu_2017B", "/mnt/hadoop/store/user/dkondrat/SingleMuon/SingleMu_2017B/190419_161448/0000/", True, 1,  4723.411) 
-SingleMu2017C = SmpNtuple("SingleMu_2017C","SingleMu_2017C", "/mnt/hadoop/store/user/dkondrat/SingleMuon/SingleMu_2017C/190419_161512/0000/", True, 1,  9631.612) 
-SingleMu2017D = SmpNtuple("SingleMu_2017D","SingleMu_2017D", "/mnt/hadoop/store/user/dkondrat/SingleMuon/SingleMu_2017D/190419_161536/0000/", True, 1,  4247.682) 
-SingleMu2017E = SmpNtuple("SingleMu_2017E","SingleMu_2017E", "/mnt/hadoop/store/user/dkondrat/SingleMuon/SingleMu_2017E/190419_161602/0000/", True, 1,  9028.733)
-SingleMu2017F = SmpNtuple("SingleMu_2017F","SingleMu_2017F", "/mnt/hadoop/store/user/dkondrat/SingleMuon/SingleMu_2017F/190419_161626/0000/", True, 1,  13443.249) # recalculate lumi!
+SingleMu2017B = DataSample("SingleMu_2017B","SingleMu_2017B", "/mnt/hadoop/store/user/dkondrat/SingleMuon/SingleMu_2017B/190419_161448/0000/", 4723.411) 
+SingleMu2017C = DataSample("SingleMu_2017C","SingleMu_2017C", "/mnt/hadoop/store/user/dkondrat/SingleMuon/SingleMu_2017C/190419_161512/0000/", 9631.612) 
+SingleMu2017D = DataSample("SingleMu_2017D","SingleMu_2017D", "/mnt/hadoop/store/user/dkondrat/SingleMuon/SingleMu_2017D/190419_161536/0000/", 4247.682) 
+SingleMu2017E = DataSample("SingleMu_2017E","SingleMu_2017E", "/mnt/hadoop/store/user/dkondrat/SingleMuon/SingleMu_2017E/190419_161602/0000/", 9028.733)
+SingleMu2017F = DataSample("SingleMu_2017F","SingleMu_2017F", "/mnt/hadoop/store/user/dkondrat/SingleMuon/SingleMu_2017F/190419_161626/0000/", 13443.249) # recalculate lumi!
 
 
 
