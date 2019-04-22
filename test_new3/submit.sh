fe8a1bffe662990e8d1fd7571b9400595be2d357
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-04-22 15:21:11 -0400

    fix

--- test_new3/submit.sh
+++ test_new3/submit.sh
@@ -9,19 +9,19 @@ case $1 in
 	# 	python test_new/test_dnn.py
 	# 	;;
 	3.1)
-		echo 'Running option 2.1'
+		echo 'Running option 3.1'
 		python test_new3/test_bdt_ucsd_hiStat.py
 		;;
 	3.2)
-		echo 'Running option 2.2'
+		echo 'Running option 3.2'
 		python test_new3/test_bdt_ucsd_hiStat_ebe.py
 		;;
 	3.3)
-		echo 'Running option 2.3'
+		echo 'Running option 3.3'
 		python test_new3/test_dnn_hiStat_m120To130_CS.py
 		;;
 	3.4)
-		echo 'Running option 2.4'
+		echo 'Running option 3.4'
 		python test_new3/test_dnn_hiStat_m120To130_noSingleMu.py
 		;;
 
