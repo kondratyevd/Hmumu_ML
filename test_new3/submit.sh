409a0ec04021aeb7073be91d214f2ca86d1b45e6
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-04-22 15:20:06 -0400

    new training options

---
+++ test_new3/submit.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+python src/prepare.py
+RunID=$(cat "output/CURRENT_RUN_ID")
+
+case $1 in
+	# 0)
+	# 	echo 'Running option 0'
+	# 	python test_new/test_dnn.py
+	# 	;;
+	3.1)
+		echo 'Running option 2.1'
+		python test_new3/test_bdt_ucsd_hiStat.py
+		;;
+	3.2)
+		echo 'Running option 2.2'
+		python test_new3/test_bdt_ucsd_hiStat_ebe.py
+		;;
+	3.3)
+		echo 'Running option 2.3'
+		python test_new3/test_dnn_hiStat_m120To130_CS.py
+		;;
+	3.4)
+		echo 'Running option 2.4'
+		python test_new3/test_dnn_hiStat_m120To130_noSingleMu.py
+		;;
+
+	*)
+		echo 'Wrong option ' $1;;		
+
+esac
+
