e8dfb22cb875d18de862d31a4e6ddaaeeff7bcc0
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-05-04 23:51:22 -0400

    test

--- cut_optimization/dynamic_categorization/categorize_dynamic_2d.py
+++ cut_optimization/dynamic_categorization/categorize_dynamic_2d.py
@@ -334,7 +334,7 @@ def callback(result):
     label, score, cut = result
     print "*"*100
     print label
-    # categories[label].set_splitting(score, cut)
+    categories[label].set_splitting(score, cut)
     # categories[label].print_structure()
 
 
@@ -352,8 +352,8 @@ for l1 in range(1, args.nSteps1+1):
                 print "Number of CPUs: ", mp.cpu_count()
                 pool = mp.Pool(mp.cpu_count())
                 a = [pool.apply_async(solve_subproblem, args = (i1,j1,i2,i2+l2-1), callback=callback) for i2 in range(0, args.nSteps2-l2+1)]
-                # for process in a:
-                #     process.wait()
+                for process in a:
+                    process.wait()
                 # pool.close()
                 # pool.join()
                 # for i2 in range(0, args.nSteps2 - l2 + 1): # j = i+l-1
