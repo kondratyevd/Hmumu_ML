b325069a555efc68659c28af7ab62e8872fd003b
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-05-05 18:33:23 -0400

    test

--- cut_optimization/dynamic_categorization/categorize_dynamic_2d.py
+++ cut_optimization/dynamic_categorization/categorize_dynamic_2d.py
@@ -379,7 +379,8 @@ framework = Categorizer(score1, args.min_var1, args.max_var1, args.nSteps1, scor
 # framework.main_loop()
 
 
-def solve_subproblem(framework, i1,j1,i2,j2):
+def solve_subproblem(i1,j1,i2,j2):
+    global framework
     print "="*50
     print "   Solving subproblem P_%i_%i_%i_%i"%(i1,j1,i2,j2)
     print "   The goal is to find best significance in category containing bins #%i through #%i by 1st variable and #%i through #%i by 2nd variable"%(i1,j1,i2,j2)
@@ -511,7 +512,7 @@ for l1 in range(1, framework.nSteps1+1):
             for i1 in range(0, framework.nSteps1 - l1 + 1): 
                 j1=i1+l1-1
                 pool = mp.Pool(mp.cpu_count())
-                a = [pool.apply_async(solve_subproblem, args = (framework, i1,j1,i2,i2+l2-1), callback=callback) for i2 in range(0, framework.nSteps2-l2+1)]
+                a = [pool.apply_async(solve_subproblem, args = (i1,j1,i2,i2+l2-1), callback=callback) for i2 in range(0, framework.nSteps2-l2+1)]
                 for process in a:
                     process.wait()
                 pool.close()
