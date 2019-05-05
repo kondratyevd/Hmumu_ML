b56b436d60ba3bba73b40d6ed8f473ce6d1bfe18
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-05-05 13:29:52 -0400

    test

--- cut_optimization/dynamic_categorization/categorize_dynamic_2d.py
+++ cut_optimization/dynamic_categorization/categorize_dynamic_2d.py
@@ -350,7 +350,7 @@ for l1 in range(1, args.nSteps1+1):
                 for process in a:
                     process.wait()
                 pool.close()
-                pool.join()
+                # pool.join()
                 for i2 in range(0, args.nSteps2 - l2 + 1): # j = i+l-1
                     j1 = i1+l1-1
                     j2 = i2+l2-1
