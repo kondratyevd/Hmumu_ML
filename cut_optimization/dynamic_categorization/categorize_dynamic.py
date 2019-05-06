dfe16473bc2203518a4581f66fa4ae27dac08479
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-05-06 18:36:25 -0400

    test

--- cut_optimization/dynamic_categorization/categorize_dynamic.py
+++ cut_optimization/dynamic_categorization/categorize_dynamic.py
@@ -8,11 +8,11 @@ import argparse
 import multiprocessing as mp
 
 # Disable most of RooFit output
-ROOT.RooMsgService.instance().getStream(1).removeTopic(ROOT.RooFit.Eval)
-ROOT.RooMsgService.instance().getStream(1).removeTopic(ROOT.RooFit.Fitting)
-ROOT.RooMsgService.instance().getStream(1).removeTopic(ROOT.RooFit.Minimization)
-ROOT.RooMsgService.instance().getStream(1).removeTopic(ROOT.RooFit.ObjectHandling)
-ROOT.RooMsgService.instance().getStream(1).removeTopic(ROOT.RooFit.NumIntegration)
+# ROOT.RooMsgService.instance().getStream(1).removeTopic(ROOT.RooFit.Eval)
+# ROOT.RooMsgService.instance().getStream(1).removeTopic(ROOT.RooFit.Fitting)
+# ROOT.RooMsgService.instance().getStream(1).removeTopic(ROOT.RooFit.Minimization)
+# ROOT.RooMsgService.instance().getStream(1).removeTopic(ROOT.RooFit.ObjectHandling)
+# ROOT.RooMsgService.instance().getStream(1).removeTopic(ROOT.RooFit.NumIntegration)
 
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('--sig_in_path', action='store', dest='sig_input_path', help='Input path')
