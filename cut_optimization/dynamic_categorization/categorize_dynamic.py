72b70c7abb65c08734bfcdd039aa2d7abaf6431c
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-05-07 18:18:02 -0400

    test

--- cut_optimization/dynamic_categorization/categorize_dynamic.py
+++ cut_optimization/dynamic_categorization/categorize_dynamic.py
@@ -107,7 +107,15 @@ if args.option is "0": # ucsd categories
         "cat2": "(bdtucsd_01jet>0.4)&(bdtucsd_01jet<0.65)&(njets<2)",
         "cat3": "(bdtucsd_01jet>0.65)&(bdtucsd_01jet<1)&(njets<2)",
     }
-    
+
+    cat_ucsd_2jet = {
+        "cat0": "(bdtucsd_2jet>-1)&(bdtucsd_2jet<0)&(njets>=2)",
+        "cat1": "(bdtucsd_2jet>0)&(bdtucsd_2jet<0.55)&(njets>=2)",
+        "cat2": "(bdtucsd_2jet>0.55)&(bdtucsd_2jet<0.85)&(njets>=2)",
+        "cat3": "(bdtucsd_2jet>0.85)&(bdtucsd_2jet<0.91)&(njets>=2)",
+        "cat4": "(bdtucsd_2jet>0.91)&(bdtucsd_2jet<1)&(njets>=2)",    
+    }
+
     file_path = "/mnt/hadoop/store/user/dkondrat/UCSD_files/"
     ggh_path = file_path+"tree_ggH.root"
     vbf_path = file_path+"tree_VBF.root"
@@ -117,7 +125,7 @@ if args.option is "0": # ucsd categories
     tt_path = file_path+"tree_top.root"
     vv_path = file_path+"tree_VV.root"
 
-    create_datacard_ucsd(cat_ucsd_01jet, ggh_path, vbf_path, vh_path, tth_path, dy_path, tt_path, vv_path, args.output_path,  "datacard", "workspace")
+    create_datacard_ucsd(cat_ucsd_2jet, ggh_path, vbf_path, vh_path, tth_path, dy_path, tt_path, vv_path, args.output_path,  "datacard", "workspace")
     os.system('pwd')
     os.system('ls')
     os.system('combine -M Significance --expectSignal=1 -t -1 -d datacard.txt')
