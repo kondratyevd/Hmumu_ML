62fbe04e4604ea88e47fd95fe735f2ec83cb6bff
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-05-07 17:20:07 -0400

    test

--- cut_optimization/dynamic_categorization/categorize_dynamic.py
+++ cut_optimization/dynamic_categorization/categorize_dynamic.py
@@ -97,6 +97,8 @@ if args.option is "0": # ucsd categories
     tt_path = file_path+"tree_top.root"
     vv_path = file_path+"tree_VV.root"
     create_datacard_ucsd(cat_ucsd_incl, ggh_path, vbf_path, dy_path, tt_path, vv_path, args.output_path,  "datacard", "workspace")
+    os.system('pwd')
+    os.system('ls')
     os.system('combine -M Significance --expectSignal=1 -t -1 -d datacard.txt')
     os.system('rm datacard.txt')
     os.system('rm workspace.root') 
