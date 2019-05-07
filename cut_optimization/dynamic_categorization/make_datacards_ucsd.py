cc755de0b3063f9b28c954a8fa3429e8c70a08f9
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-05-06 20:40:05 -0400

    test

--- cut_optimization/dynamic_categorization/make_datacards_ucsd.py
+++ cut_optimization/dynamic_categorization/make_datacards_ucsd.py
@@ -101,9 +101,9 @@ def add_bkg_model(w, cat_name, dy_path, tt_path, vv_path, cut):
         return 0, bkg_entries
 
 
-    w.factory("%s_a1 [1.66, 0.01, 2.1]"%cat_name)
-    w.factory("%s_a2 [0.39, 0.30, 1]"%cat_name)
-    w.factory("%s_a3 [-0.26, -2, -0.01]"%cat_name)
+    w.factory("%s_a1 [1.66, 0.7, 2.1]"%cat_name)
+    w.factory("%s_a2 [0.39, 0.30, 0.62]"%cat_name)
+    w.factory("%s_a3 [-0.26, -0.40, -0.12]"%cat_name)
     w.factory("expr::%s_bwz_redux_f('(@1*(@0/100)+@2*(@0/100)^2)',{hmass, %s_a2, %s_a3})"%(cat_name,cat_name,cat_name))
     w.factory("EXPR::%s_bkg('exp(@2)*(2.5)/(pow(@0-91.2,@1)+pow(2.5/2,@1))',{hmass, %s_a1, %s_bwz_redux_f})"%(cat_name,cat_name,cat_name))
     fit_func = w.pdf('%s_bkg'%cat_name)
