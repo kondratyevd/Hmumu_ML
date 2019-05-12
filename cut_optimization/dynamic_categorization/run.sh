2209fc5cae2e7ed85900654ade8a1bb8225aa4a5
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-05-12 17:11:42 -0400

    0%

--- cut_optimization/dynamic_categorization/run.sh
+++ cut_optimization/dynamic_categorization/run.sh
@@ -217,7 +217,7 @@ case $1 in
 		min_var=-1
 		max_var=1
 		nSteps=200
-		penalty=1
+		penalty=0
 		;;	
 
 	6.4)
@@ -229,7 +229,7 @@ case $1 in
 		min_var=-1
 		max_var=1
 		nSteps=200
-		penalty=1
+		penalty=0
 		;;	
 
 	6.5)
@@ -241,7 +241,7 @@ case $1 in
 		min_var=-1
 		max_var=1
 		nSteps=200
-		penalty=1
+		penalty=0
 		;;
 
 	0)
