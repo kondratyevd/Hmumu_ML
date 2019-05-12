860cf714c5e7e8dc977cbc141170c069124a0ef1
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-05-12 14:34:39 -0400

    test

--- cut_optimization/dynamic_categorization/make_datacards_ucsd.py
+++ cut_optimization/dynamic_categorization/make_datacards_ucsd.py
@@ -27,6 +27,7 @@ def add_sig_model(w, cat_name, sig_path_list, cut):
     for path in sig_path_list:
         signal_tree.Add(path)
         print "Adding path "+path
+        print "Now there are %i entries"%signal_tree.GetEntries()
     signal_tree.SetName("signal_tree")
 
     signal_hist_name = "signal_%s"%cat_name
