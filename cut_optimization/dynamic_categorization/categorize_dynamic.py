fe16ae76cfb0711cfb507b31fa03744694e152d4
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-05-14 16:18:24 -0400

    test

--- cut_optimization/dynamic_categorization/categorize_dynamic.py
+++ cut_optimization/dynamic_categorization/categorize_dynamic.py
@@ -224,7 +224,7 @@ if args.option is "0": # ucsd categories
         "cat5": "(bdtucsd_2jet_bveto>0.94)&(bdtucsd_2jet_bveto<1)&(njets>=2)",        
     }
 
-    create_datacard_ucsd(cat_ucsd_2jet_bveto, sig_2016+sig_2017+sig_2018, bkg_2016+bkg_2017+bkg_2018, args.output_path,  "datacard", "workspace")
+    create_datacard_ucsd(my_best_2jet_bveto, sig_2016+sig_2017+sig_2018, bkg_2016+bkg_2017+bkg_2018, args.output_path,  "datacard", "workspace")
     os.system('pwd')
     os.system('ls')
     os.system('combine -M Significance --expectSignal=1 -t -1 -d datacard.txt')
