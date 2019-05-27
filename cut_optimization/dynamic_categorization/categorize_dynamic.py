a4115464872e5b1de625b4008df85cf9e811de67
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-05-27 14:20:42 -0400

    mjj cut 350

--- cut_optimization/dynamic_categorization/categorize_dynamic.py
+++ cut_optimization/dynamic_categorization/categorize_dynamic.py
@@ -243,11 +243,11 @@ if args.option is "0": # ucsd categories
     }
 
     my_best_2jet_bveto_mjjcut = {
-        "cat0": "(bdtucsd_2jet_bveto>-1)&(bdtucsd_2jet_bveto<-0.65)&(njets>=2)&(mjj<300)",
-        "cat1": "(bdtucsd_2jet_bveto>-0.65)&(bdtucsd_2jet_bveto<-0.35)&(njets>=2)&(mjj<300)",
-        "cat3": "(bdtucsd_2jet_bveto>-0.35)&(bdtucsd_2jet_bveto<0.02)&(njets>=2)&(mjj<300)",
-        "cat4": "(bdtucsd_2jet_bveto>0.02)&(bdtucsd_2jet_bveto<0.53)&(njets>=2)&(mjj<300)",
-        "cat5": "(bdtucsd_2jet_bveto>0.53)&(bdtucsd_2jet_bveto<1)&(njets>=2)&(mjj<300)",   
+        "cat0": "(bdtucsd_2jet_bveto>-1)&(bdtucsd_2jet_bveto<-0.72)&(njets>=2)&(mjj<350)",
+        "cat1": "(bdtucsd_2jet_bveto>-0.72)&(bdtucsd_2jet_bveto<-0.28)&(njets>=2)&(mjj<350)",
+        "cat3": "(bdtucsd_2jet_bveto>-0.28)&(bdtucsd_2jet_bveto<0.05)&(njets>=2)&(mjj<350)",
+        "cat4": "(bdtucsd_2jet_bveto>0.05)&(bdtucsd_2jet_bveto<0.53)&(njets>=2)&(mjj<350)",
+        "cat5": "(bdtucsd_2jet_bveto>0.53)&(bdtucsd_2jet_bveto<1)&(njets>=2)&(mjj<350)",   
     }
 
     create_datacard_ucsd(my_best_2jet_bveto_mjjcut, sig_2016+sig_2017+sig_2018, bkg_2016+bkg_2017+bkg_2018, args.output_path,  "datacard", "workspace")
