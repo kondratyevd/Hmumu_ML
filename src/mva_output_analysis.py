e1e4c58c51e482354349d0c7666d54a563b6ca9a
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-06-07 17:53:59 -0400

    test

--- src/mva_output_analysis.py
+++ src/mva_output_analysis.py
@@ -788,6 +788,15 @@ dnn_sigloss_1layer_roc_graph = dnn_sigloss_1layer.plot_roc("log(sig_prediction/(
 dnn_sigloss_1layer_roc = a.RocCurve(dnn_sigloss_1layer_roc_graph, "dnn_sigloss_1layer", "DNN sigloss 1 layer", ROOT.kGreen, 1)
 roc_to_compare.append(dnn_sigloss_1layer_roc)
 
+dnn_sigloss_1layer_reg = a.add_mva_source("DNN_sigloss_1layer_reg", "DNN sigloss 1 layer_reg", "/home/dkondra/tmp/Run_2019-06-07_15-03-46//Keras/model_sigloss/root/")
+dnn_sigloss_1layer_reg.add_sample("bkg", "Background", "output_t*root", "tree_background", False, True, ROOT.kOrange-3, False)
+dnn_sigloss_1layer_reg.add_sample("sig", "Signal", "output_t*root", "tree_signal", False, False, ROOT.kRed, False)
+dnn_sigloss_1layer_reg.set_lumi(4723.411)
+# dnn_sigloss_1layer_reg_roc_graph = dnn_sigloss_1layer_reg.plot_roc("log(((1-2*pow(10,-10))*sig_prediction+pow(10,-10))/(1-((1-2*pow(10,-10))*sig_prediction+pow(10,-10))))", 500, -100, 100, [0.5])
+dnn_sigloss_1layer_reg_roc_graph = dnn_sigloss_1layer_reg.plot_roc("log(sig_prediction/(1-sig_prediction))", 500, -100, 100, [0.5])
+dnn_sigloss_1layer_reg_roc = a.RocCurve(dnn_sigloss_1layer_reg_roc_graph, "dnn_sigloss_1layer_reg", "DNN sigloss 1 layer_reg", ROOT.kViolet, 1)
+roc_to_compare.append(dnn_sigloss_1layer_reg_roc)
+
 dnn_sigloss_3layers = a.add_mva_source("DNN_sigloss_3layers", "DNN sigloss 3 layers", "/home/dkondra/tmp/Run_2019-06-06_15-53-36//Keras/model_sigloss/root/")
 dnn_sigloss_3layers.add_sample("bkg", "Background", "output_t*root", "tree_background", False, True, ROOT.kOrange-3, False)
 dnn_sigloss_3layers.add_sample("sig", "Signal", "output_t*root", "tree_signal", False, False, ROOT.kRed, False)
