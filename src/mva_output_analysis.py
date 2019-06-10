f7436b91f6d8000ad2cd5799bae95662b583bd52
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2019-06-10 11:25:55 -0400

    test

--- src/mva_output_analysis.py
+++ src/mva_output_analysis.py
@@ -797,7 +797,7 @@ dnn_sigloss_1layer_reg.add_sample("bkg", "Background", "output_t*root", "tree_ba
 dnn_sigloss_1layer_reg.add_sample("sig", "Signal", "output_t*root", "tree_signal", False, False, ROOT.kRed, False)
 dnn_sigloss_1layer_reg.set_lumi(4723.411)
 # dnn_sigloss_1layer_reg_roc_graph = dnn_sigloss_1layer_reg.plot_roc("log(((1-2*pow(10,-10))*sig_prediction+pow(10,-10))/(1-((1-2*pow(10,-10))*sig_prediction+pow(10,-10))))", 500, -100, 100, [0.5])
-dnn_sigloss_1layer_reg_roc_graph = dnn_sigloss_1layer_reg.plot_roc("sig_prediction", 10000, 0, 1, [0.5])
+dnn_sigloss_1layer_reg_roc_graph = dnn_sigloss_1layer_reg.plot_roc("sig_prediction", 100000, 0, 1, [0.5])
 dnn_sigloss_1layer_reg_roc = a.RocCurve(dnn_sigloss_1layer_reg_roc_graph, "dnn_sigloss_1layer_reg", "DNN sigloss 1 layer_reg", ROOT.kViolet, 1)
 roc_to_compare.append(dnn_sigloss_1layer_reg_roc)
 
